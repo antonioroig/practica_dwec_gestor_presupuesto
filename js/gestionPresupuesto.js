a151f1b13362e929b5ced9de01c29edd29cefe28
Pablo Berruezo <pabbergal@alu.edu.gva.es>
2022-10-05 20:35:33 +0200

    05102022 actividades fundamentos js

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,62 @@
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
-// TODO: Variable global
-
-
-function actualizarPresupuesto() {
-    // TODO
+var presupuesto = 0;
+function actualizarPresupuesto(num_int) 
+{
+    
+    if(num_int >= 0 && typeof num_int === 'number'){
+        presupuesto = num_int;
+        return presupuesto;
+    }
+    else
+    {
+        console.log('el número' +num_int + ' no es válido ');
+        return -1;
+    }    
+ 
 }
 
-function mostrarPresupuesto() {
-    // TODO
+
+function mostrarPresupuesto() 
+{
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion,valor_actual) 
+{
+    this.descripcion = descripcion;
+
+    if(valor_actual > 0 && typeof valor_actual ==='number')
+    {
+
+        this.valor = valor_actual;
+
+    }
+    else
+    {
+        this.valor = 0;
+    };
+    
+    this.mostrarGasto = function ()
+    {
+        return 'Gasto correspondiente a '+this.descripcion+ ' con valor ' +this.valor +' €';
+    };
+    
+    this.actualizarDescripcion = function(cadena_act)
+    {
+        this.descripcion = cadena_act;
+    };
+
+    this.actualizarValor = function(val_actualizado)
+    {
+        if(val_actualizado >= 0)
+        {
+            this.valor = val_actualizado;
+        }
+        else
+        {
+            console.log(`El valor introducido es negativo, no se ha podido actualizar`);
+        };
+    };
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
@@ -23,3 +67,4 @@ export   {
     actualizarPresupuesto,
     CrearGasto
 }
+
