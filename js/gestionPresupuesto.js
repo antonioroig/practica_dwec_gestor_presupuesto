61ec18f101c2bfc5419a1e19d7ccb8284830b9ae
Franco Mattiazzo <framat2@alu.edu.gva.es>
2022-10-05 20:17:15 +0200

    nuevo repositorio correcto

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,61 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
+"use strict";
 // TODO: Variable global
-
-
-function actualizarPresupuesto() {
+let presupuesto = 0;
+function actualizarPresupuesto(valor) {
+    if (valor >= 0 && typeof valor === 'number')
+    {
+        presupuesto = valor;
+        return presupuesto
+    }
+    else {
+        console.log("El valor debe ser mayor o igual a 0");
+        return -1;
+    }
     // TODO
 }
 
 function mostrarPresupuesto() {
+    return ("Tu presupuesto actual es de " + presupuesto + " €")
     // TODO
 }
 
-function CrearGasto() {
-    // TODO
+
+
+function CrearGasto(descripcion, valor) {
+    this.descripcion = descripcion
+    if (valor <= 0 || typeof valor !== 'number')
+    {
+        valor = 0;
+    }
+
+    this.valor = valor
+    this.descripcion = descripcion
+    this.mostrarGasto = function mostrarGasto () {
+        return ("Gasto correspondiente a " + descripcion + " con valor " + this.valor + " €")
+    }
+    
+    
+    this.actualizarDescripcion = function actualizarDescripcion(nuevadescripcion)
+    {
+        if (typeof nuevadescripcion === 'string')
+        {
+            this.descripcion = nuevadescripcion;
+        }
+    }
+    this.actualizarValor = function actualizarValor(valornuevo)
+    {
+        if (valornuevo >= 0 && typeof valornuevo === 'number')
+        {
+            this.valor = valornuevo;
+        }
+    }
+
+    // TODO   
+    //comentario para que se activen los test en github
 }
+
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
