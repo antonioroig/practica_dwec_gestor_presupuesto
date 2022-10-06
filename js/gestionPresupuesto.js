cd486d2868ec03487a12f7ab1f5c9e1c7f703824
Samy <sambendje@alu.edu.gva.es>
2022-10-06 17:23:38 +0200

    commit1

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,56 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
-
+"use strict";
 // TODO: Variable global
+var presupuesto = 0;
 
-
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    if(valor >= 0  && typeof valor === `number`)
+    {
+        presupuesto = valor;
+        return valor;
+    }
+    else
+    {
+        console.log(`error, El numero es menor que 0`);
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return (`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(pdescripcion,pvalor) {
+
+    if(pvalor < 0 || typeof(pvalor) !== `number`)
+    {
+        pvalor = 0; 
+    }
+        
+        this.descripcion= `${pdescripcion}`,
+        this.valor = pvalor,
+
+
+    this.mostrarGasto = function()
+    {
+        return (`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
+    },
+    
+    this.actualizarDescripcion  = function(descripcion)
+    {
+        this.descripcion = descripcion;
+    },
+
+    this.actualizarValor = function(valor)
+    {
+        if(valor >= 0 && typeof(valor) === `number`)
+        {
+            this.valor = valor;
+        }
+    } 
+        
 }
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
