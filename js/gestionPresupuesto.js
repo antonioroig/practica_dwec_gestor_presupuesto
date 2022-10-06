b725f2211baaf98bd45633134bbd16cd173980cd
Smarlin Jimenez <smajimgom@alu.edu.gva.es>
2022-10-06 19:58:04 +0200

    practica 0 y 1

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,53 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
-
-
-function actualizarPresupuesto() {
+let presupuesto = 0;
+function actualizarPresupuesto(valorIntroducido) {
     // TODO
+    let aux = "";
+    if(valorIntroducido >= 0)
+    {
+        presupuesto = valorIntroducido;
+        return presupuesto;
+
+    }else 
+    {
+        console.log("Error, se ha introducido un valor negativo");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
-}
+        let mostrarP ="Tu presupuesto actual es de " + presupuesto + " €";
+        return mostrarP;
+    }
 
-function CrearGasto() {
+function CrearGasto(descripcion,valor) {
     // TODO
-}
+    this.descripcion=descripcion;
+    if (valor <0 && typeof valor==='number') {
+        this.valor = 0;
+    }else 
+    {
+        this.valor = valor;
+    }
+    this.mostrarGasto = function()
+    {
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    }
+    this.actualizarDescripcion =function (descrip)
+    {
+        this.descripcion=descrip;
+    }
+    this.actualizarValor = function(valoact) 
+    {
+        if (valoact >= 0) 
+        {
+            this.valor = valoact;
+        }
+    }
+}   
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
---
+++ js/presentacionNavegador.js
@@ -0,0 +1,1 @@
+alert("Hola Smarlin Jiménez Gómez")
