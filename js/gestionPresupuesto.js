368f5c6b8d528b7419b6ea09e19166ea25a6791f
Juanma Paterna <juapatzar@alu.edu.gva.es>
2022-10-06 19:25:48 +0200

    Primer commit del nuevo

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,55 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
-
-
-function actualizarPresupuesto() {
+let presupuesto = 0;
+actualizarPresupuesto(-5);
+console.log(presupuesto);
+function actualizarPresupuesto(number) {
     // TODO
+    if(number >= 0 && typeof number === "number"){
+    presupuesto = number;
+    return number;
+    }   
+    else{
+    console.log("Error");
+    return -1;
+    }
+
 }
+
 
 function mostrarPresupuesto() {
     // TODO
+   return (`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
-function CrearGasto() {
+
+function CrearGasto(pdescripcion,pvalor) {
     // TODO
+    if(pvalor < 0 || typeof(pvalor) !== `number`){
+        pvalor = 0;
+    }
+    
+        this.valor = pvalor,
+        this.descripcion = `${pdescripcion}`,
+
+        this.mostrarGasto = function(){
+           return (`Gasto correspondiente a ${pdescripcion} con valor ${pvalor} €`)
+        },
+        this.actualizarDescripcion = function(descripcion){
+            this.descripcion = descripcion;
+        },
+        this.actualizarValor = function(pvalor){
+            if(pvalor >= 0 && typeof pvalor === `number`){
+                this.valor = pvalor;
+            }
+        
+    }
+    
+
+    
 }
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
---
+++ js/presentacionNavegador.js
@@ -0,0 +1,1 @@
+alert(`Juan Manuel Paterna Zarandona`);
