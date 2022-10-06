36586b5c28cc0b13fc4cb9a90856b7089bed6dff
Mohamed Tajni <mohtaj@alu.edu.gva.es>
2022-10-06 20:54:12 +0200

    cambios pr js1

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,25 +1,65 @@
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
+
+
 
 // TODO: Variable global
+let presupuesto = 0 ;
+ 
+function actualizarPresupuesto(dinero)
+{
+   if (typeof dinero ==="number" && dinero > 0 ){
+ 
+        return presupuesto = dinero;
+ 
+    }else
+    {
+       console.log("-1");
+       return -1;
+    }
+    // TODO
+}
+ 
+function mostrarPresupuesto()
+{
+    return `Tu presupuesto actual es de ${presupuesto} €`;
+    // TODO
+}
+let valor;
+ 
+function CrearGasto(descripcion,valor) {
 
+    this.descripcion = descripcion;
 
-function actualizarPresupuesto() {
+    if (typeof this.valor ==="number" > 0)
+    {
+        this.valor =valor;
+    }else{
+        this.valor=0;
+    }
+  
+   
     // TODO
 }
 
-function mostrarPresupuesto() {
-    // TODO
+
+function mostrarGasto(){
+    let descripcion = CrearGasto.descripcion;
+    return (`Gasto correspondiente a ${descripcion} con valor VALOR €, siendo VALOR y DESCRIPCION`)
+
 }
 
-function CrearGasto() {
-    // TODO
-}
 
+
+
+
+ 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    mostrarGasto
 }
+ 
+ 
