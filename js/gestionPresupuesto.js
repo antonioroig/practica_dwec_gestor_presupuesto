6312abbd6455fad9e07a0a5153a5eabe7c220974
CalderonSerrano <joscalser@alu.edu.gva.es>
2022-10-06 19:56:34 +0200

    Nuevo

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,17 +2,45 @@
 
 // TODO: Variable global
 
+var presupuesto = 0;
 
-function actualizarPresupuesto() {
-    // TODO
+function actualizarPresupuesto(valor) {
+    if(valor >= 0)
+    {
+        return presupuesto = valor;
+    }
+    else
+    {
+        console.log("Error.\nHa introducido un valor negativo");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor) {
+    this.descripcion = descripcion;
+    if(valor > -1)
+    {
+        this.valor = valor;
+    }
+    else{
+        this.valor = 0;
+    }
+    this.mostrarGasto = function(){
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    };
+    this.actualizarDescripcion = function(nueva_descripcion){
+        this.descripcion = nueva_descripcion;
+    }
+    this.actualizarValor = function(nuevo_valor){
+        if(nuevo_valor > -1)
+        {
+            this.valor = nuevo_valor;
+        }
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
