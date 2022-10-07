604923eeb0f1b2557adad005b709cc0651731e2d
Alvaro Montesinos <alvmonfer@alu.edu.gva.es>
2022-10-07 20:41:03 +0200

    Practica 0 y 1 terminadas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,59 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
 
-function actualizarPresupuesto() {
+function actualizarPresupuesto(num) {
     // TODO
+    if(num >= 0 && typeof num === 'number')
+    {
+        presupuesto = num;
+        return presupuesto;
+    }
+    else
+    {
+        console.log('ERROR, el numero introducido es negativo');
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return 'Tu presupuesto actual es de '+presupuesto+' €';
 }
 
-function CrearGasto() {
+function CrearGasto(descripcion,valor) 
+{
     // TODO
+    this.descripcion = descripcion;
+    if(valor > 0 && !isNaN(valor))
+    {
+        this.valor = valor;
+    }
+    else
+    {
+        this.valor = 0;
+    };
+    this.mostrarGasto = function()
+    {
+        return "Gasto correspondiente a " +this.descripcion+" con valor "+this.valor+" €";
+    };
+    this.actualizarValor = function(nwvalor)
+    {
+        if(nwvalor >= 0)
+        {
+            this.valor = nwvalor
+        }
+        else
+        {
+            console.log('El valor introducido no es valido, no ha podido ser cambiado')
+        };
+    };
+    this.actualizarDescripcion = function(nwdesc)
+    {
+        this.descripcion = nwdesc;
+    };
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
