b9dc93f025ba67e14b95a573b713d72022db80df
Tomas Barrera <tombar2@alu.edu.gva.es>
2022-10-07 22:09:10 +0200

    realizada practica js

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,18 +2,56 @@
 
 // TODO: Variable global
 
+"use strict"
 
-function actualizarPresupuesto() {
-    // TODO
+var presupuesto = 0;
+
+function actualizarPresupuesto(numero) {
+    if(numero > 0){
+        presupuesto = numero;
+        return numero;
+    }else{
+        console.log("error: el numero es menor que 0");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return (`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion,valor) {
+
+    if(valor < 0 || typeof(valor) !== `number`)
+    {
+        valor = 0; 
+    }
+
+        this.descripcion= `${descripcion}`,
+        this.valor = valor,
+
+
+    this.mostrarGasto = function()
+    {
+        return (`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
+    },
+    //
+    this.actualizarDescripcion  = function(descripcion)
+    {
+        this.descripcion = descripcion;
+    },
+    //
+    this.actualizarValor = function(valor)
+    {
+        if(valor >= 0 && typeof(valor) === `number`)
+        {
+            this.valor = valor;
+        }
+    } 
+    //
 }
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
