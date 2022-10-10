dbc20988cacc8d849f6c5ff2fe70a45e17712ce9
Sergio Cobos <sercobjua@alu.edu.gva.es>
2022-10-10 18:41:43 +0200

    Actualizacion Parametros Rest

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,6 +2,8 @@
 
 // TODO: Variable global
 var presupuesto = 0;
+var gastos = [];
+var idGasto = 0;
 
 function actualizarPresupuesto(nuevoPresupuesto) {
     if (nuevoPresupuesto >= 0)
@@ -19,15 +21,21 @@ function mostrarPresupuesto() {
     return ('Tu presupuesto actual es de ' + presupuesto + ' €');
 }
 
-function CrearGasto(descripcion,valor) {
+function CrearGasto(descripcion,valor,fecha,...etiquetas) {
     this.descripcion=descripcion;
     this.valor=0;
+    this.fecha=new Date(timestamp);
+    this.etiquetas = [];
+    if(etiquetas != undefined){
+        this.etiquetas=etiquetas;
+    }
     if(valor >= 0){
         this.valor=valor;
     };
 
     this.mostrarGasto = function(){
-        return (`Gasto correspondiente a ` + this.descripcion + ` con valor ` + this.valor + ` €`);
+        return (`Gasto correspondiente a ` + this.descripcion + ` con valor ` + this.valor + ` €. \n
+        Fecha: ` + this.fecha.toLocaleString() + `\n Etiquetas: \n`);
     }
 
     this.actualizarDescripcion = function(nuevaDescripcion){
@@ -40,13 +48,38 @@ function CrearGasto(descripcion,valor) {
             this.valor = nuevoValor;
         }
     }
+
+    this.actualizarFecha = function(fecha){
+        if(typeof fecha == 'string'){
+            this.fecha = Date.parse(fecha);
+        }
+    }
+
+    this.anyadirEtiquetas() = function(){
+
+    }    
 }
 
+
+function listarGastos(){
+    return gastos;
+}
+function anyadirGasto(idGasto){
+
+}
+function borrarGasto(){}
+function calcularTotalGastos(){}
+function calcularBalance(){}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
