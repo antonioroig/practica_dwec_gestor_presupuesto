0b1025ce0ca04ad5cb90b6374b7b6d0f84249e68
Hellen <heltitbai@edu.alu.gva.es>
2022-10-10 20:08:10 +0200

    inicio práctica 2

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,6 +2,8 @@
 "use strict";
 // TODO: Variable global
 let presupuesto = 0;
+let idGasto = 0;
+let gastos = new Array();
 
 function actualizarPresupuesto(valor) {
     if(valor >= 0 && typeof valor === 'number')
@@ -18,15 +20,31 @@ function actualizarPresupuesto(valor) {
 function mostrarPresupuesto(){
     return `Tu presupuesto actual es de ${presupuesto} €`;
 }
-function CrearGasto(descripcion, valor) { //Función constructora, tiene que empezar por mayus
+function CrearGasto(descripcion, valor, fecha, etiquetas) { //Función constructora, tiene que empezar por mayus
+    
     this.descripcion = descripcion;
+    this.fecha = new Date();
+
     if(valor >= 0 && typeof valor === 'number'){
         this.valor = valor;
     }
     else{
         this.valor = 0;
     }
+    // Revisar
+    if(etiquetas != null){
+        this.etiquetas = etiquetas;
+    }else{
+        this.etiquetas = new Array();
+    }
     
+    // Revisar
+    if(fecha != null && Date.parse(fecha)){
+        this.fecha = fecha;
+    }else{
+        this.fecha = now.getDate();
+    }
+
     //Métodos:
     this.mostrarGasto = function(){
         return(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
@@ -42,14 +60,46 @@ function CrearGasto(descripcion, valor) { //Función constructora, tiene que emp
         }
     }
 }
+// Revisar
+function listarGastos(){
+    return gastos;
+}
+//Revisar
+function anyadirGasto(id){
+    id = idGasto;
+    idGasto++;
+}
+// Revisar
+function borrarGasto(id){
 
+    gastos.forEach((id ) => {
+        delete gastos[id]
+    })
 
+}
+// check 
+function calcularTotalGastos(){
+
+    let valorTotal = 0 ;
+    for (let i = 0;i < gastos.length; i++) {
+        valorTotal += gastos.valor;
+    }
+    return valorTotal;
+};
+function calcularBalance(){
+
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
 
