af33ea758b9d03cfd2d5c8beb3456122cce08860
JaviGva <javbernav@alu.edu.gva.es>
2022-10-10 20:42:20 +0200

    variables locales y creacion de funciones

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,8 +2,8 @@
 
 // TODO: Variable global
 var presupuesto = 0;
-
-
+var gastos = new Array();
+var idGasto = 0;
 
 function actualizarPresupuesto(nuevoPresupuesto) {
     if(typeof nuevoPresupuesto==='number' && nuevoPresupuesto>=0){
@@ -44,11 +44,36 @@ function CrearGasto(descripcion, valor) {
 
 }
 
+function listarGastos(){
+    return gastos;
+}
+
+function anyadirGasto(){
+
+}
+
+function borrarGasto(){
+
+}
+
+function calcularTotalGastos(){
+
+}
+
+function calcularBalance(){
+
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto, 
+    borrarGasto, 
+    calcularTotalGastos, 
+    calcularBalance
 }
