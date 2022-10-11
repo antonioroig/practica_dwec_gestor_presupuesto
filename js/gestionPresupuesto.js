14cdd04a14bb2af1e12e3b08f74815200759c4a7
Ainhoa Izquierdo <ainizqare@alu.edu.gva.es>
2022-10-11 20:45:03 +0200

    arrays en la practica 3 JS2

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,6 +2,9 @@
 "use strict"; 
 // TODO: Variable global
  let presupuesto = 0;
+ let gastos =[];
+ let idGasto = 0;
+ 
 
 function actualizarPresupuesto(valor) {
     // TODO
@@ -22,10 +25,13 @@ function mostrarPresupuesto() {
     return(`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
-function CrearGasto(descripcion,valor) {
+function CrearGasto(descripcion,valor,fecha,etiquetas) {
     // TODO
+    this.fecha = new Date();
+    this.etiquetas = [];
+
     this.descripcion = descripcion;
-    
+
     if ((valor >= 0 && typeof valor === 'number')) 
     {
        this.valor = valor;
@@ -53,8 +59,50 @@ function CrearGasto(descripcion,valor) {
         {
             this.valor = nuevoValor;
         }     
+    },
+
+    this.mostrarGastoCompleto = function(){
+       
+        return(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.\nFecha: ${this.fecha.toLocaleString()}\nEtiquetas:\n${etiquetas}`);
+    
+    },
+    this.actualizarFecha = function(nuevaFecha){
+
+    },
+
+    this.anyadirEtiquetas = function(nuevasEtiquetas){
+
+    },
+    
+    this.borrarEtiquetas = function(){
+
     }
-} 
+
+}
+
+function listarGastos(){
+    return gastos;
+};
+
+function anyadirGasto(gasto){
+
+};
+
+function borrarGasto(id){
+
+};
+
+function calcularTotalGastos(){
+
+};
+
+function calcularBalance(){
+
+};
+
+
+
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -62,6 +110,12 @@ export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
     CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
+    
 }
 
 
