e68eaf49a9d3b963982dd264b43f065efe9e28d0
Raul Gonzalez <raugonbon@alu.edu.gva.es>
2022-10-13 18:16:49 +0200

    commit practica 2 - 1

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -3,8 +3,9 @@
 // TODO Variable global
 let presupuesto = 0;
 
-/*let gastos = new Array[0];*/
 
+let gastos = new Array();
+let idGasto = 0; /* un contador de los gastos */
 
 function actualizarPresupuesto(valor) {
     // TODO
@@ -59,13 +60,37 @@ function CrearGasto(descripcion, valor) {
     };
     
 }
+/* - - - - - - - - - - - - - - - - */
+//++++
+function listarGastos(){
+    return gastos;
+}
 
+function anyadirGasto(){
 
+}
+function borrarGasto(){
+
+}
+function calcularTotalGastos(){
+
+}
+function calcularBalance (){
+
+}
 /* NO MODIFICAR A PARTIR DE AQUÍ exportación de funciones y objetos creados para poder ejecutar los tests.
 Las funciones y objetos deben tener los nombres que se indican en el enunciado
 Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo */
 export   {
    mostrarPresupuesto,
    actualizarPresupuesto,
-   CrearGasto
+   CrearGasto,
+   listarGastos,
+   anyadirGasto,
+   borrarGasto,
+   calcularTotalGastos,
+   calcularBalance
+
+
+
 }
