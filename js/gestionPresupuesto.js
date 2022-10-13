662ded890d291e44b6626b724b81fc0228960c68
Smarlin Jimenez <smajimgom@alu.edu.gva.es>
2022-10-13 20:37:30 +0200

    empezando la practica 2
    añadiendo las funciones vacias y modificando el objeto export

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,6 +2,8 @@
 
 // TODO: Variable global
 let presupuesto = 0;
+let gastos;
+let idGasto;
 function actualizarPresupuesto(valorIntroducido) {
     // TODO
     let aux = "";
@@ -47,13 +49,33 @@ function CrearGasto(descripcion,valor) {
             this.valor = valoact;
         }
     }
-}   
+} 
+ 
+function listarGastos(){
 
+}
+function anyadirGasto(){
+    
+}
+function borrarGasto(){
+    
+}
+function calcularTotalGastos(){
+    
+}
+function calcularBalance(){
+    
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
+    listarGastos, 
+    anyadirGasto, 
+    borrarGasto, 
+    calcularTotalGastos,
+    calcularBalance,
     CrearGasto
 }
