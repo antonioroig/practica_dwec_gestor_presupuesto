d6a43ae113af8b2f7bf016527d3d6870f9023a39
dibez <diebelzam@alu.edu.gva.es>
2022-10-14 16:19:41 +0200

    Función listarGastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,6 +2,8 @@
 
 // TODO: Variable global
 var presupuesto = 0;
+var gastos = new Array();
+var idGasto = 0;
 
 function actualizarPresupuesto(valor) {
     // TODO
@@ -42,11 +44,32 @@ function CrearGasto(descripcionGasto, valorGasto) {
         }
     }
 }
+
+function listarGastos(){
+    return gastos;
+}
+function anyadirGasto(){
+
+}
+function borrarGasto(){
+
+}
+function calcularTotalGastos(){
+
+}
+function calcularBalance(){
+
+}
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
