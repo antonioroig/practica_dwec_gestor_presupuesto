ca523d12c8c38255d8a4e2f4849477acfdaff17e
David Llinares <davlliesc@alu.edu.gva.es>
2022-10-14 16:59:18 +0200

    4 pruebas pasadas

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -19,21 +19,21 @@ function mostrarPresupuesto() {
     return(`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
-function CrearGasto(pdescripcion, pvalor, pfecha = Date.now(),... petiquetas) {
+function CrearGasto(pdescripcion, pvalor, pfecha = Date.now(), ... petiquetas) {
     // TODO
     if(typeof pvalor !== 'number' || pvalor <= 0)
     {
             pvalor = 0;
     }
-    if(petiqueta == undefined){
+    if([...petiquetas] === undefined){
         this.etiquetas = [];
     } else{
-        this.etiquetas = petiquetas;
+        this.etiquetas = [...petiquetas];
     }
-    if(!isNaN(Date.parse(fecha))){
-        this.fecha == Date.now();//poner fecha actual
+    if(isNaN(Date.parse(pfecha))){
+        this.fecha = Date.now();//poner fecha actual
     } else{
-        this.fecha = Date.Parse(pfecha);
+        this.fecha = Date.parse(pfecha);
     }
     
         
@@ -55,10 +55,38 @@ function CrearGasto(pdescripcion, pvalor, pfecha = Date.now(),... petiquetas) {
                 }
                 
             }
-            this.anyadirEtiquetas = function(){
+            this.anyadirEtiquetas = function(... petiquetas){
+                let esta = 0
+                petiquetas.forEach((pitem) =>{
+                    esta = 0,
+                    this.etiquetas.forEach((item) =>{
+                        if(item === pitem)
+                        {
+                            esta = 1
+                        }
+                    })
+                    if(esta === 0){
+                        this.etiquetas.push(petiquetas)
+                    }
+                });
                 
             }
-    
+            this.mostrarGastoCompleto = function(){
+                let etiqtxt = '';
+                
+                this.etiquetas.forEach((item) =>{
+                    
+                        etiqtxt = etiqtxt + "- " + item + "\n";
+                    
+                });
+                return(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €.` + "\n" + `Fecha: ${new Date(this.fecha).toLocaleString()}` + "\n" + "Etiquetas:\n" + `${etiqtxt}`)
+            
+            }
+            this.actualizarFecha = function(pfecha){
+                if(!isNaN(Date.parse(pfecha))){
+                    this.fecha = Date.parse(pfecha);
+                }
+            }
     
         
 }
