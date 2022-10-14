f6e33e99d59c4ff999248d8046072c3d229db952
Aurora Alemañ <auralefus@alu.edu.gva.es>
2022-10-14 20:49:47 +0200

    Corrección errores y algunos métodos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -21,17 +21,22 @@ function mostrarPresupuesto() {
     return(`Tu presupuesto actual es de ${presupuesto} €`);
 }
 
-function CrearGasto(descripcion, valor, fecha, etiquetas) { //Función constructora, tiene que empezar por mayus
+function CrearGasto(descripcion, valor, fecha=Date.now() , ...etiquetas) { //Función constructora, tiene que empezar por mayus
     this.descripcion = descripcion;
     this.fecha = new Date();
-    this.etiquetas = [];
+    this.etiquetas = [...etiquetas];
     if(valor >= 0 && typeof valor === 'number'){
         this.valor = valor;
     }
     else{
         this.valor = 0;
     }
-    
+    if(typeof fecha === 'string' && !isNaN(Date.parse(fecha))){
+        this.fecha = Date.parse(fecha);
+    }
+    else{
+        this.fecha = Date.now();
+    }
     //Métodos:
     this.mostrarGasto = function(){
         return(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`);
@@ -48,16 +53,22 @@ function CrearGasto(descripcion, valor, fecha, etiquetas) { //Función construct
     },
 
     this.mostrarGastoCompleto = function(){
-
+        return(`Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €. \nFecha: ${this.fecha.toLocaleString()}. \nEtiquetas:\n${this.etiquetas}`);
     },
     this.actualizarFecha = function(nueva_fecha){
-
+        if(typeof nueva_fecha === 'string' && !isNaN(Date.parse(nueva_fecha))){
+            this.fecha = Date.parse(nueva_fecha);
+        }
     },
     this.anyadirEtiquetas = function(nueva_etiqueta){
-
+        nueva_etiqueta.forEach(element => {
+            if(this.etiquetas.includes(element)){
+                this.etiquetas.push(element);
+            }
+        });
     },
     this.borrarEtiquetas = function(){
-
+        this.etiquetas = [...etiquetas];
     }
 }
 
@@ -65,16 +76,22 @@ function listarGastos(){
     return gastos;
 };
 function anyadirGasto(gasto){
-
+    gasto.id = idGasto; //También puedes hacer gasto[id] = idGasto
+    idGasto++;
+    gastos.push(gasto);
 };
 function borrarGasto(id){
 
 };
 function calcularTotalGastos(){
-
+    let total_gasto = 0;
+    gastos.forEach(gasto => {
+        total_gasto = total_gasto + gasto.valor;
+    });
+    return total_gasto;
 };
 function calcularBalance(){
-
+    return presupuesto - calcularTotalGastos();
 };
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
