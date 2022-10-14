aeb6c21de1128143333c3fd80f183d966f01b791
Alvaro Montesinos <alvmonfer@alu.edu.gva.es>
2022-10-14 19:16:51 +0200

    comienzo practica 3

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,7 +2,8 @@
 
 // TODO: Variable global
 let presupuesto = 0;
-
+let gastos = [];
+let idGastos = 0;
 
 function actualizarPresupuesto(num) {
     // TODO
@@ -23,10 +24,13 @@ function mostrarPresupuesto() {
     return 'Tu presupuesto actual es de '+presupuesto+' €';
 }
 
-function CrearGasto(descripcion,valor) 
+function CrearGasto(descripcion,valor,fecha,...etiqs) 
 {
     // TODO
+    let now = new Date();
+
     this.descripcion = descripcion;
+
     if(valor > 0 && !isNaN(valor))
     {
         this.valor = valor;
@@ -35,6 +39,9 @@ function CrearGasto(descripcion,valor)
     {
         this.valor = 0;
     };
+
+    if(fecha )
+    
     this.mostrarGasto = function()
     {
         return "Gasto correspondiente a " +this.descripcion+" con valor "+this.valor+" €";
@@ -56,11 +63,41 @@ function CrearGasto(descripcion,valor)
     };
 }
 
+function listarGastos()
+{
+    return gastos;
+}
+
+function anyadirGasto()
+{
+
+}
+
+function borrarGasto()
+{
+
+}
+
+function calcularTotalGastos()
+{
+
+}
+
+function calcularBalance()
+{
+
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto, 
+    borrarGasto, 
+    calcularTotalGastos,
+    calcularBalance
 }
