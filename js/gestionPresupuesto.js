0a46431ca719ed16ef89f2f58383c719b5bf04bb
Pablo Sirvent <pabsircan@alu.edu.gva.es>
2022-10-19 19:22:47 +0200

    ultimos cambios pr2 19/10/22

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -2,7 +2,9 @@
 
 // TODO: Variable global
 
+let gastos = new Array();
 let presupuesto = 0;
+let idGasto = 0;
 
 
 function actualizarPresupuesto(parametroAct) {
@@ -27,9 +29,30 @@ function mostrarPresupuesto() {
 }
 
 // revisar
-function CrearGasto(descripcion, valor) {
+function CrearGasto(descripcion, valor, fecha=Date.now().getTime(), ...etiquetas) {
     // TODO
     this.descripcion = descripcion;
+    let gasto1 = new crearGasto ();
+    let fecha = new Date(0);
+    this.fecha = fecha;
+    this.etiquetas = new Array();
+
+
+    if (etiquetas === null){
+        this.etiquetas = etiquetas;
+    }
+
+    if (Date.parse(fecha)){
+        this.fecha = fecha.getTime();
+    }
+    
+    else{
+        this.fecha = Date.now().getTime();
+    }
+
+    if (typeof fecha === ' string' && fecha !== NaN(Date.parse(fecha))){
+        this.fecha = Date.parse(fecha);
+    }
 
     if (valor >= 0 && !isNaN(valor))
     {
@@ -61,11 +84,55 @@ function CrearGasto(descripcion, valor) {
         }
     } 
 }
+
+function listarGastos(){
+
+    return gastos;
+}
+
+function anyadirGasto(gasto1){
+
+    gasto1.id = idGasto; /* otra manera de añadir seria gasto1[id] */
+    idGasto++;
+    gastos = gasto1.push();
+}
+
+/* 
+this.etiquetas = etiquetas;
+this. etiquetas = [...etiquetas];
+*/
+
+function borrarGasto(id){
+
+    gastos.splice(id,1);
+
+}
+
+function calcularTotalGastos(){
+
+    let ret = gastos.reduce((a, b) => a + b, 0);
+    console.log(ret);
+
+    return ret;
+}
+
+function calcularBalance(){
+    
+    let Tgastos = calcularTotalGastos();
+
+
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
