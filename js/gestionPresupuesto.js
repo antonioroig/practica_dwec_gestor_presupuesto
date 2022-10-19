87d1f683be3bf326458fc44732c6355accdb4b56
Andrea Zoughbi Cano <andzoucan@alu.edu.gva.es>
2022-10-19 19:16:37 +0200

    creación de variables y añadir a export

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,4 +1,7 @@
 let presupuesto=0;
+let gastos= new Array();
+idGasto=0;
+
 
 function actualizarPresupuesto(PresAct) {
     if (typeof PresAct === "number" && PresAct>=0){
@@ -44,11 +47,35 @@ function CrearGasto(Descripcion, valor) {
     };
 };
 
+function listarGastos() {
+
+};
+function anyadirGasto () {
+
+};
+
+function borrarGasto() {
+
+};
+
+function calcularTotalGastos() {
+
+};
+
+function calcularBalance() {
+
+};
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos, 
+    anyadirGasto, 
+    borrarGasto, 
+    calcularTotalGastos, 
+    calcularBalance
 }
