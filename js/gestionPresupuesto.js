4c6ba7a5478dccbbc427ddedb1e1d5f764601b45
Sergio Cobos <sercobjua@alu.edu.gva.es>
2022-10-21 16:40:19 +0200

    Arreglos Practica2

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -39,7 +39,7 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
     };
 
     this.mostrarGasto = function(){
-        return (`Gasto correspondiente a ` + this.descripcion + ` con valor ` + this.valor + ` €.`);
+        return (`Gasto correspondiente a ` + this.descripcion + ` con valor ` + this.valor + ` €`);
     }
 
     this.mostrarGastoCompleto = function(){
