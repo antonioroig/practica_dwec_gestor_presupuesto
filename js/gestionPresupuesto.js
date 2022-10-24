554c03e9115a1866c81e421d93ffb73f01f0afd8
JaviGva <javbernav@alu.edu.gva.es>
2022-10-24 20:47:21 +0200

    funcion filtargastos mal

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -85,6 +85,29 @@ function CrearGasto(descripcion, valor, fecha, ...etiquetas) {
         });
     }
 
+    this.obtenerPeriodoAgrupacion = function(periodo){
+        let devolver = new Date(this.fecha).getFullYear();
+        if(periodo==="anyo"){
+            return devolver;
+        }
+        devolver+="-";
+        if(new Date(this.fecha).getMonth()+1<10){
+            devolver+="0";
+        }
+        devolver+=new Date(this.fecha).getMonth()+1;
+        if(periodo==="mes"){
+            return devolver;
+        }
+        devolver+="-";
+        if(new Date(this.fecha).getDate()<10){
+            devolver+="0";
+        }
+        devolver+=new Date(this.fecha).getDate();
+        if(periodo==="dia"){
+            return devolver;
+        }
+    }
+
 }
 
 function listarGastos(){
@@ -117,6 +140,48 @@ function calcularBalance(){
     return presupuesto - calcularTotalGastos();
 }
 
+
+function filtrarGastos({fechaDesde, fechaHasta, valorMaximo, valorMinimo, descripcionContiene}){
+    if(Object.entries(gastoNuevo).length===0){
+        return gastos;
+    }else{
+        if(isNaN(Date.parse(fechaDesde))){
+            fechaDesde=null;
+        }
+        if(isNaN(Date.parse(fechaHasta))){
+            fechaHasta = null;
+        }
+        let gastosCorrectos = gastos.filter(function (gasto){
+            let esCorrecto = true;
+                    if(gasto.fecha < Date.parse(fechaDesde) || fechaDesde===null){
+                        esCorrecto=false;
+                    }
+                    if(gasto.fecha > Date.parse(gastoNuevo.fechaHasta) || gastoNuevo.fechaDesde===null){
+                        esCorrecto=false;
+                    }
+                    if(gasto.valor < gastoNuevo.valorMinimo){
+                        esCorrecto=false;
+                    }
+                    if(gasto.valor > gastoNuevo.valorMaximo){
+                        esCorrecto=false;
+                    }
+                    if(!gasto.descripcion.includes(gastoNuevo.descripcionContiene)){
+                        esCorrecto=false;
+                    }
+        
+                    return esCorrecto;
+        });
+        return gastosCorrectos;
+    }
+    
+}
+
+
+
+function agruparGastos(){
+
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -128,5 +193,7 @@ export   {
     anyadirGasto, 
     borrarGasto, 
     calcularTotalGastos, 
-    calcularBalance
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos
 }
