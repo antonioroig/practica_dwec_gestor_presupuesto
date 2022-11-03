70f54856920a70dcfe0836a5e71078bfa5fa23f9
Raul Gonzalez <raugonbon@alu.edu.gva.es>
2022-11-03 19:59:51 +0100

    P4 - 1 Commit

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,23 @@
+
+
+
+function mostrarDatoEnId(){
+
+}
+function mostrarGastoWeb(){
+
+}
+function mostrarGastosAgrupadosWeb(){
+
+}
+
+
+
+// npx cypress open -- PARA HACER TEST GRÁFICO
+// npm run test --> pasa todos los tests
+// EXPORT - EXPORT - EXPORT - EXPORT - EXPORT - EXPORT - EXPORT - EXPORT - EXPORT - EXPORT - EXPORT - EXPORT
+export   {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+ }
