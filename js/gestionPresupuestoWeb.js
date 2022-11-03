d1aba580eb71072d8ad47e2dd64a6ea760b47d96
Smarlin Jimenez <smajimgom@alu.edu.gva.es>
2022-11-03 20:22:53 +0100

    iniciando la practica 4

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,24 @@
+function mostrarDatoEnId(idElemento, valor)
+{
+    let elem = document.getElementById(idElemento);
+    let p = document.createElement("p");
+    p.textContent = valor;
+    elem.appendChild(p);
+}
+function mostrarGastoWeb()
+{
+    
+}
+function mostrarGastosAgrupadosWeb()
+{
+    
+}
+
+
+export   { 
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
+//npm cypress open//para ejecutar el text de la practica 4//
+//type module//
