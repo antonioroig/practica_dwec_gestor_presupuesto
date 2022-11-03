84b27c4794cae1dd38bd6cb9177a7182579855c2
Samy Elyes <sambendje@alu.edu.gva.es>
2022-11-03 20:36:13 +0100

    cypress no funciona

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,37 @@
+
+
+let mostrarDatoEnId = function(valor,idElemento){
+    let elem = document.getElementById('idElemento');
+    elem.textContent= valor;
+}
+
+
+let mostrarGastoWeb = function(){
+
+}
+
+let mostrarGastosAgrupadosWeb = function(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export   {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
