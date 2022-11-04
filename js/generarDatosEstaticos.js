f551d4ebc63aaa654c3ae332e8dce9885a0e3882
Pol Aran <113632539+AreSzk@users.noreply.github.com>
2022-11-04 16:40:59 +0100

    Importar desde GP

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,19 @@
+import * as gp from './gestionPresupuesto';
+
+function mostrarDatoEnId(idElemento, valor) {
+    
+}
+  
+function mostrarGastoWeb(idElemento, gasto) {
+
+}
+
+function mostrarGastosAgrupadosWeb(idElemento, agrup) {
+
+}
+
+export {
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+};
