5af295bed5f768dd4ef8e0b0af700e08f4c3f0b7
Juanma Paterna <juapatzar@alu.edu.gva.es>
2022-11-04 19:05:56 +0100

    terminado mostrar dato en id

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,2 +1,25 @@
-import * as ges from './gestionPresupuesto.js';
-import * as gesPW from './gestionPresupuestoWeb.js';
+import * as ges from "./gestionPresupuesto.js";
+import * as gesPW from "./gestionPresupuestoWeb.js";
+
+ges.actualizarPresupuesto(1500);
+
+gesPW.mostrarDatoEnId(ges.mostrarPresupuesto(),"presupuesto");
+
+let gasto1 = new ges.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+let gasto2 = new ges.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+let gasto3 = new ges.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+let gasto4 = new ges.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+let gasto5 = new ges.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+let gasto6 = new ges.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+ges.anyadirGasto(gasto1);
+ges.anyadirGasto(gasto2);
+ges.anyadirGasto(gasto3);
+ges.anyadirGasto(gasto4);
+ges.anyadirGasto(gasto5);
+ges.anyadirGasto(gasto6);
+
+gesPW.mostrarDatoEnId(ges.calcularTotalGastos(),"gastos-totales");
+gesPW.mostrarDatoEnId(ges.calcularBalance(),"balance-total");
+
+ges.listarGastos();
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,8 +1,11 @@
-import * as ges from './gestionPresupuesto.js';
+import * as ges from "./gestionPresupuesto.js";
+
 
 function mostrarDatoEnId(valor,idElemento){
-    let elemento = document.getElementById(idElemento);
-    elemento.innerHTML += valor;
+    if(idElemento !== undefined){
+        let elemento = document.getElementById(idElemento);
+        elemento.innerHTML += " " + valor;
+    }
 }
 
 function mostrarGastoWeb(idElemento,gasto){
