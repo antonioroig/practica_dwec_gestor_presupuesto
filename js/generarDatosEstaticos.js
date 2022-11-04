b43ebb800550ea091ad55e46536e8b224eb70d63
Jeronimo Belardinelli <jerbel@alu.edu.gva.es>
2022-11-04 19:11:55 +0100

    Terminada funcion mostrar id

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,14 +1,27 @@
-import * as gestion from "./gestionPresupuesto";
-import * as gestionWeb from "./gestionPresupuestoWeb";
+import * as gestion from "./gestionPresupuesto.js";
+import * as gestionWeb from "./gestionPresupuestoWeb.js";
 
 gestion.actualizarPresupuesto(1500);
 
 gestionWeb.mostrarDatoEnId(gestion.mostrarPresupuesto(),"presupuesto");
 
 
-gestion.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
-gestion.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
-gestion.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
-gestion.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
-gestion.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
-gestion.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+let gasto1 =new gestion.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+let gasto2 = new gestion.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+let gasto3 = new gestion.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+let gasto4 = new gestion.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+let gasto5 = new gestion.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+let gasto6 = new gestion.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+gestion.anyadirGasto(gasto1);
+gestion.anyadirGasto(gasto2);
+gestion.anyadirGasto(gasto3);
+gestion.anyadirGasto(gasto4);
+gestion.anyadirGasto(gasto5);
+gestion.anyadirGasto(gasto6);
+
+gestionWeb.mostrarDatoEnId(gestion.calcularTotalGastos(),"gastos-totales");
+
+gestionWeb.mostrarDatoEnId(gestion.calcularBalance(),"balance-total");
+
+gestion.listarGastos();
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,22 +1,37 @@
-import * as gestion from './gestionPresupuesto';
+import * as gestion from './gestionPresupuesto.js';
 
 function mostrarDatoEnId(valor,idElemento){
     
     if(idElemento !== undefined){
         let id = document.getElementById(idElemento);
         id.innerHTML += " " + valor;
-    }  
+    }
 }
 
-function mostrarGastoWeb(){}
+function mostrarGastoWeb(idElemento, gasto){
+    let id = document.getElementById(idElemento);
+
+    let divGasto = document.createElement("div");
+    let divGastoDes = document.createElement("div");
+    divGastoDes.className = "gasto-descripcion";
+    divGastoDes.textContent(gasto.descripcion);
+
+
+    let divGastoFecha = document.createElement("div");
+    divGastoFecha.textContent(gasto.fecha);
+
+
+    let divGastoValor = document.createElement("div");
+    divGastoValor.textContent = document.createElement(gasto.valor);
+
+    for(let i = 0; i < gasto.etiqueta.lenght;i++){
+        let span = document.createElement("span");
+        span.className = "gasto-etiquetas-etiqueta";
+    }
+
+}
 
 function mostrarGastosAgrupadosWeb(){}
-
-
-
-
-
-
 
 
 export	{
