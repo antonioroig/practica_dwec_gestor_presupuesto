7d755319fbcd4aa39e69446d175fb3bba5da7789
David Llinares <davlliesc@alu.edu.gva.es>
2022-11-04 15:50:15 +0100

    creados los documentos

---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,26 @@
+//gestionPresupuestoWeb.js
+
+/*
+mostrarDatoEnId
+mostrarGastoWeb
+mostrarGastosAgrupadosWeb
+*/
+import{mostrarPresupuesto} from  "./gestionPresupuesto";
+function mostrarDatoEnId(valor, idElemento)
+{
+
+}
+function mostrarGastoWeb(idElemento, gasto)
+{
+
+}
+function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo)
+{
+    
+}
+
+export{
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
