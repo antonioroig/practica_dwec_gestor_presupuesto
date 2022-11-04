72155a4cfecda07719701aaea61badefaa488903
dibez <diebelzam@alu.edu.gva.es>
2022-11-04 18:13:29 +0100

    error cypress plugin file

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,4 +1,5 @@
 import * as gestionPresupuesto from './gestionPresupuesto.js';
+
 function mostrarDatoEnId(idElemento, valor){
     let elemento = Document.getElementById(idElemento);
     elemento.innerHtml(valor);
