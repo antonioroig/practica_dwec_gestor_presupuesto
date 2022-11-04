0b026b02a45049e885efb76769d4e95e610586b6
Hellen <heltitbai@edu.alu.gva.es>
2022-11-04 17:48:23 +0100

    fin desarrollo funcion mostrarDatoId

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,5 +1,23 @@
 import * as gestionWeb from '../js/gestionPresupuestoWeb.js';
-import * as gestion from '../js/gestionPresupuesto';
-
-
+import * as gestion from '../js/gestionPresupuesto.js';
+// Check
 gestion.actualizarPresupuesto(1500);
+gestionWeb.mostrarDatoEnId(gestion.mostrarPresupuesto(),"presupuesto");
+let objeto1 = new gestion.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+let objeto2 = new gestion.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+let objeto3 = new gestion.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+let objeto4 = new gestion.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+let objeto5 = new gestion.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+let objeto6 =  new gestion.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+gestion.anyadirGasto(objeto1);
+gestion.anyadirGasto(objeto2);
+gestion.anyadirGasto(objeto3);
+gestion.anyadirGasto(objeto4);
+gestion.anyadirGasto(objeto5);
+gestion.anyadirGasto(objeto6);
+
+gestionWeb.mostrarDatoEnId(gestion.calcularTotalGastos(),"gastos-totales");
+gestionWeb.mostrarDatoEnId(gestion.calcularBalance(),"balance-total");
+
+// Revisar
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,15 +1,20 @@
 
-function mostrarDatoEnId(idElemento, valor){
-    let id = document.getElementById(idElemento);
+function mostrarDatoEnId(valor, idElemento){
+    let element = document.getElementById(idElemento);
+    element.innerHTML += `<p>${valor}</p>`;
+    /*let parrafo = document.createElement("p");
+    parrafo.textContent = valor;
+    element.appendChild(parrafo);*/
+
     //  let p = document.createElement('p');
-    id.innerHTML += `<p>${valor}</p>`;
+    // id.innerHTML += `<p>${valor}</p>`;
 
     /*let div = document.createElement('div');
     div.innerHTML = `<div id='${idElemento}'>${valor.toString()}</div>`;
     document.body.append(div);*/
     // document.write(`<div id='${idElemento}'>${valor}</div>`);
 }
-function mostrarGastoWeb(idElemento, gasto){
+function mostrarGastoWeb(gasto, idElemento){
     let elemento = document.getElementById(idElemento);
     let div = document.createElement('div');
     div.
