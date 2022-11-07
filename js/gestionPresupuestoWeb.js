216680eb9a879608d8b0932e6d174ded3e1bba40
David Llinares <davlliesc@alu.edu.gva.es>
2022-11-07 19:56:22 +0100

    probando con la funcion mostar gasto web

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -19,7 +19,42 @@ function mostrarDatoEnId(valor, idElemento)
 
 function mostrarGastoWeb(idElemento, gasto)
 {
+    if (idElemento != undefined)
+    {
+        
+        let elemento = document.getElementById(idElemento);
+        let divGasto = document.createElement('div');
+        divGasto.className = "gasto";
+        let divGastoDescripcion = document.createElement('div');
+        divGastoDescripcion.className = "gasto-descripcion";
+        divGastoDescripcion.innerHTML+=gasto.descricpion;
+        divGasto.append(divGastoDescripcion);
 
+        let divGastoFecha = document.createElement('div');
+        divGastoFecha.className = "gasto-fecha";
+        divGastoFecha.innerHTML+=gasto.fecha;
+        divGasto.append(divGastoFecha);
+
+        let divGastoValor = document.createElement('div');
+        divGastoValor.className = "gasto-valor";
+        divGastoValor.innerHTML+=gasto.valor;
+        divGasto.append(divGastoValor);
+
+        let divGastoEtiquetas = document.createElement('div');
+        divGastoEtiquetas.className = "gasto-etiquetas";
+        
+        for(let i = 0; i < gasto.etiquetas.length; i++){
+            let divGastoEtiquetasEtiqueta = document.createElement('div');
+            divGastoEtiquetasEtiqueta.className = "gasto-etiquetas-etiqueta";
+            divGastoEtiquetasEtiqueta.innerHTML += gasto.etiquetas[i];
+            divGastoEtiquetasEtiqueta.innerHTML += "</span>";
+            divGastoEtiquetas.append(divGastoEtiquetasEtiqueta);
+        }
+        divGasto.append(divGastoEtiquetas);
+        
+        
+
+    }
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo)
