42f283ccad4fbc6bd14b31762c2213723fe004be
Piero <piesor@alu.edu.gva.es>
2022-11-08 20:48:46 +0100

    Primer commit de la practica

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,12 @@
+
+import * as gesPres from './gestionPresupuesto';
+import * as gesWeb from '.gestionPresupuestoWeb';
+
+
+
+gesPres.actualizarPresupuesto(num)
+{
+    num = 1500;
+
+    return ("El presupuesto se ha actualizado a " + num + "€")
+}
---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,23 @@
+
+
+
+import * as gasto from './gestionPresupuesto';
+
+
+
+function mostrarDatoEnId(idElemento, valor)
+{
+
+
+
+
+}
+
+
+
+
+export   {
+mostrarDatoEnId,
+mostrarGastoWeb,
+mostrarGastosAgrupadosWeb
+}
