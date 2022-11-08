379ffbc5d38e230296da5c24bbec3ec6bc806495
Raul Gonzalez <raugonbon@alu.edu.gva.es>
2022-11-08 17:36:10 +0100

    P4 - Filtros de mostrarGastoWeb

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -16,19 +16,34 @@ gestionPresupuesto.anyadirGasto(new gestionPresupuesto.CrearGasto("Seguro coche"
 gestionPresupuestoWeb.mostrarDatoEnId( gestionPresupuesto.calcularTotalGastos(), 'gastos-totales'); // mostramos gastos totales
 gestionPresupuestoWeb.mostrarDatoEnId( gestionPresupuesto.calcularBalance(), 'balance-total'); // mostramos balance total
 
+//Mostrar el listado completo de gastos en div#listado-gastos-completo (funciones listarGastos y mostrarGastoWeb)
 let gastosListados = gestionPresupuesto.listarGastos();
-
 for(let i = 0; i < gastosListados.length; i++){
     //console.log(gastosListados);
     gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-completo', gastosListados[i]);
 }
+// Mostrar el listado de gastos realizados en septiembre de 2021 en div#listado-gastos-filtrado-1 (funciones filtrarGastos y mostrarGastoWeb)
+let gastosFiltrados = gestionPresupuesto.filtrarGastos({fechaDesde: '2021-09-01', fechaHasta: '2021-09-30'});
+for(let i = 0; i < gastosFiltrados.length; i++){
+    gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-1', gastosFiltrados[i]);
+}
+// Mostrar el listado de gastos de más de 50€ en div#listado-gastos-filtrado-2 (funciones filtrarGastos y mostrarGastoWeb)
+gastosFiltrados = gestionPresupuesto.filtrarGastos({valorMinimo: 50});
+for(let i = 0; i < gastosFiltrados.length; i++){
+    gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-2', gastosFiltrados[i]);
+}
+// Mostrar el listado de gastos de más de 200€ con etiqueta seguros en div#listado-gastos-filtrado-3 (funciones filtrarGastos y mostrarGastoWeb)
+gastosFiltrados = gestionPresupuesto.filtrarGastos({valorMaximo: 200, etiquetasTiene: ['seguros']});
+for(let i = 0; i < gastosFiltrados.length; i++){
+    gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-3', gastosFiltrados[i]);
+}
+// Mostrar el listado de gastos que tengan las etiquetas comida o transporte de menos de 50€ en div#listado-gastos-filtrado-4 (funciones filtrarGastos y mostrarGastoWeb)
+gastosFiltrados = gestionPresupuesto.filtrarGastos({valorMaximo: 50, etiquetasTiene: ['comida','transporte']})
+for(let i = 0; i < gastosFiltrados.length; i++){
+    gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-4', gastosFiltrados[i]);
+}
 
 /* 
-Mostrar el listado completo de gastos en div#listado-gastos-completo (funciones listarGastos y mostrarGastoWeb)
-Mostrar el listado de gastos realizados en septiembre de 2021 en div#listado-gastos-filtrado-1 (funciones filtrarGastos y mostrarGastoWeb)
-Mostrar el listado de gastos de más de 50€ en div#listado-gastos-filtrado-2 (funciones filtrarGastos y mostrarGastoWeb)
-Mostrar el listado de gastos de más de 200€ con etiqueta seguros en div#listado-gastos-filtrado-3 (funciones filtrarGastos y mostrarGastoWeb)
-Mostrar el listado de gastos que tengan las etiquetas comida o transporte de menos de 50€ en div#listado-gastos-filtrado-4 (funciones filtrarGastos y mostrarGastoWeb)
 Mostrar el total de gastos agrupados por día en div#agrupacion-dia (funciones agruparGastos y mostrarGastosAgrupadosWeb)
 Mostrar el total de gastos agrupados por mes en div#agrupacion-mes (funciones agruparGastos y mostrarGastosAgrupadosWeb)
 Mostrar el total de gastos agrupados por año en div#agrupacion-anyo (funciones agruparGastos y mostrarGastosAgrupadosWeb)
