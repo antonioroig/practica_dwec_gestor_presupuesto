027a069884c96f673725b5223a9e363744ce2083
Sergio Cobos <sercobjua@alu.edu.gva.es>
2022-11-17 18:59:14 +0100

    intento de arreglo

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -17,7 +17,7 @@ function mostrarDatoEnId(valor, idElemento){
         elemento.innerHTML += valor;
     }
 }
-function mostrarGastoWeb(idElemento, {descripcion, valor, fecha, ...etiquetas}){
+function mostrarGastoWeb(idElemento, gasto){
 
     if(idElemento != null){
         let elemento = document.getElementById(idElemento);
@@ -25,36 +25,33 @@ function mostrarGastoWeb(idElemento, {descripcion, valor, fecha, ...etiquetas}){
         let divGasto = document.createElement("div");
             divGasto.className = "gasto";
             elemento.appendChild(divGasto);
-        if(descripcion){
+        if(gasto.descripcion){
             let divGastoDescripcion = document.createElement("div");
                 divGastoDescripcion.className = "gasto-descripcion";
-                divGastoDescripcion.innerHTML += descripcion;
+                divGastoDescripcion.innerHTML += gasto.descripcion;
                 divGasto.appendChild(divGastoDescripcion);
         }
-        if(valor){
+        if(gasto.valor){
             let divGastoFecha = document.createElement("div");
                 divGastoFecha.className = "gasto-fecha";
-                divGastoFecha.innerHTML += fecha;
+                divGastoFecha.innerHTML += gasto.fecha;
                 divGasto.appendChild(divGastoFecha);
         }
-        if(fecha){
+        if(gasto.fecha){
             let divGastoValor = document.createElement("div");
                 divGastoValor.className = "gasto-valor";
-                divGastoValor.innerHTML += valor;
+                divGastoValor.innerHTML += gasto.valor;
                 divGasto.appendChild(divGastoValor);
         }
-        if(etiquetas){
             let divGastoEtiquetas = document.createElement("div");
                 divGastoEtiquetas.className = "gasto-etiquetas";
-                divGasto.appendChild(divGastoEtiquetas);
-                for(let i = 0; i < etiquetas.lenght; i++ ){
-                    console.log(etiquetas.lenght)
+                for(let i = 0; i < gasto.etiquetas.lenght; i++ ){
                     let spanEtiqueta = document.createElement("span");
                     spanEtiqueta.className = "gasto-etiquetas-etiqueta";
-                    spanEtiqueta.innerHTML += etiquetas[i];
+                    spanEtiqueta.innerHTML = gasto.etiquetas[i];
                     divGastoEtiquetas.appendChild(spanEtiqueta);
                 }
-        }
+                divGasto.appendChild(divGastoEtiquetas);
     }
 }
 
@@ -72,21 +69,23 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
                 divAgrupacion.appendChild(titPeriodo);
         }
         if(agrup){
-                for(const property in agrup){
+                let keys = Object.keys(agrup);
+                let i = 0;
+                for(let property in agrup){
                     let divAgrup = document.createElement("div");
                         divAgrup.className = "agrupacion-dato";
-                        divAgrupacion.appendChild(divAgrup);
                     let spanAgrupClave = document.createElement("span");
                         spanAgrupClave.className = "agrupacion-dato-clave";
-                        spanAgrupClave.innerHTML += property;
+                        spanAgrupClave.innerHTML += keys[i];
+                        divAgrup.appendChild(spanAgrupClave);
                     let spanAgrupValor = document.createElement("span");
                         spanAgrupValor.className = "agrupacion-dato-valor";
                         spanAgrupValor.innerHTML += agrup[property];
+                        divAgrup.appendChild(spanAgrupValor);
+                    
+                    divAgrupacion.appendChild(divAgrup);
+                    i++;
                 }
-
-                /*
-                for...in
-                */
         }
     }
 
