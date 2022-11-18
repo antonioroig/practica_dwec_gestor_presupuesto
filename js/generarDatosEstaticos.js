18ca5ddbaba353bb26c18b27081d02580e699e54
Hellen <heltitbai@edu.alu.gva.es>
2022-11-18 16:09:35 +0100

    avance 18/11

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -3,12 +3,6 @@ import * as gestion from '../js/gestionPresupuesto.js';
 // Check
 gestion.actualizarPresupuesto(1500);
 gestionWeb.mostrarDatoEnId(gestion.mostrarPresupuesto(),"presupuesto");
-/*let objeto1 = new gestion.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
-let objeto2 = new gestion.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
-let objeto3 = new gestion.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
-let objeto4 = new gestion.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
-let objeto5 = new gestion.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
-let objeto6 =  new gestion.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");*/
 
 gestion.anyadirGasto(new gestion.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida"));
 gestion.anyadirGasto(new gestion.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida"));
@@ -21,6 +15,11 @@ gestionWeb.mostrarDatoEnId(gestion.calcularTotalGastos(),"gastos-totales");
 gestionWeb.mostrarDatoEnId(gestion.calcularBalance(),"balance-total");
 
 // Revisar
+// gestionWeb.mostrarGastoWeb(gestion.filtrarGastos({fechaDesde:"2021-09-01"}),"listado-gastos-filtrado-1");
+// gestionWeb.mostrarGastoWeb(gestion.filtrarGastos({valorMinimo: 50}),"listado-gastos-filtrado-2");
+// gestionWeb.mostrarGastoWeb(gestion.filtrarGastos({valorMinimo:200}),"listado-gastos-filtrado-2");
+// gestionWeb.mostrarGastoWeb(gestion.filtrarGastos({etiquetasTiene: ["comida", "transporte"]}),"listado-gastos-filtrado-4");
+
 let gastos = gestion.listarGastos()
 gastos.forEach((element)=> {
     gestionWeb.mostrarGastoWeb(element,"listado-gastos-completo");
@@ -41,9 +40,3 @@ let filtrado4 = gestion.filtrarGastos({etiquetasTiene: ["comida", "transporte"]}
 filtrado4.forEach((element) => {
     gestionWeb.mostrarGastoWeb(element,"listado-gastos-filtrado-4")
 })
-// gestionWeb.mostrarGastoWeb(gestion.filtrarGastos({fechaDesde:"2021-09-01"}),"listado-gastos-filtrado-1");
-// gestionWeb.mostrarGastoWeb(gestion.filtrarGastos({valorMinimo: 50}),"listado-gastos-filtrado-2");
-// gestionWeb.mostrarGastoWeb(gestion.filtrarGastos({valorMinimo:200}),"listado-gastos-filtrado-2");
-// gestionWeb.mostrarGastoWeb(gestion.filtrarGastos({etiquetasTiene: ["comida", "transporte"]}),"listado-gastos-filtrado-4");
-
-
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,44 +1,21 @@
+// Muestra en un div el valor que se le pasa por parámetro
+
 function mostrarDatoEnId(valor, idElemento){
     let element = document.getElementById(idElemento);
     element.innerHTML += `<p>${valor}</p>`;
     
 }
 function mostrarGastoWeb(gasto, idElemento){
+
     let elemento = document.getElementById(idElemento);
 
-    /*for (const [key, value] of Object.entries(gasto)) {
-      if(!Array.isArray(value) &&  !key.includes('function')){
-
-        let div = document.createElement('div');
-        div.classList.add(`gasto-${key}`);
-        div.textContent = value;
-        elemento.appendChild(div);
-
-        // elemento.innerHTML = `<div class="gasto-${key}">${value}</div>`;
-      }else{
-
-        let etiquetas = document.createElement('div');
-        etiquetas.classList.add("gasto-etiquetas-etiqueta");
-
-        for(let j of value){
-          let span = document.createElement('span')
-          span.textContent += j
-          etiquetas.appendChild(span)
-        }
-
-        elemento.appendChild(etiquetas);
-      }
-    }*/
-      
     for(let i in gasto){
-      if(!Array.isArray(i) && !i.includes('function')){
+      if(!Array.isArray(i)){
 
         let div = document.createElement('div');
         div.classList.add(`gasto-${i}`);
-        div.textContent = gasto[i];
-        elemento.appendChild(div);
+        div.textContent = i;
 
-        // elemento.innerHTML = `<div class="gasto-${key}">${value}</div>`;
       }else{
 
         let etiquetas = document.createElement('div');
@@ -52,40 +29,19 @@ function mostrarGastoWeb(gasto, idElemento){
 
         elemento.appendChild(etiquetas);
       }
-
-      /*if(!Array.isArray(gasto[i]))
-      {
-        elemento.innerHTML = `<div class="gasto-${i}">${gasto[i]}</div>`;
-      }else{
-        for(let j in gasto[i]){
-          let span= document.createElement('span');
-          span.contains = j;
-        }
-        elemento.innerHTML(span);
-      }*/
     }
+
 }
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
-    let elemento = document.getElementById(idElemento);
-  
-
+   
 }
 
 
+export    {
 
+  mostrarDatoEnId,
+  mostrarGastoWeb,
+  mostrarGastosAgrupadosWeb
 
-
-
-
-
-
-
-
-
-
-export  {
-    mostrarDatoEnId,
-    mostrarGastoWeb,
-    mostrarGastosAgrupadosWeb
 }
 
