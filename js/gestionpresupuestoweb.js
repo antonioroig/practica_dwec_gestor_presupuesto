dec1a1bd54686705cd68eded7d3a8822669765f3
nicmas4 <113632686+nicmas4@users.noreply.github.com>
2022-11-22 18:16:25 +0100

    base de mostrarGastoWeb

--- js/gestionpresupuestoweb.js
+++ js/gestionpresupuestoweb.js
@@ -16,8 +16,34 @@ function mostrarDatoEnId(valor, idElemento){
    elemento.innerHTML(valor)
 }
 
-function mostrarGastoWeb(){
+function mostrarGastoWeb(idElemento, gasto){
+    let elemento = document.getElementById(idElemento);
     
+    let gastoD = document.createElement("div");
+    gastoD.classList = "gasto";
+    elemento.appendChild(gastoD)
+    
+    let elementoGasto = document.getElementsByClassName("gasto")
+
+    let gastoDesc = document.createElement("div");
+    gastoDesc.classList = "gasto-descripcion";
+    gastoDesc.textContent = gasto.descripcion;
+    elementoGasto.appendChild(gastoDesc)
+
+    let gastoFecha = document.createElement("div");
+    gastoFecha.classList = "gasto-fecha";
+    gastoFecha.textContent = gasto.descripcion;
+    elementoGasto.appendChild(gastoFecha)
+
+    let gastoValor = document.createElement("div");
+    gastoValor.classList = "gasto-valor";
+    gastoValor.textContent = gasto.descripcion;
+    elementoGasto.appendChild(gastoValor)
+
+    let gastoEtiquetas = document.createElement("div");
+    gastoFecha.classList = "gasto-etiquetas";
+    gastoFecha.textContent = gasto.descripcion;
+    elementoGasto.appendChild(gastoFecha)
 }
 
 function mostrarGastosAgrupadosWeb(){
