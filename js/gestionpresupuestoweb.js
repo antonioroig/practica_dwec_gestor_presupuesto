ec865d4b416c0d37df6dc485c7301970d2a6a8db
nicmas4 <113632686+nicmas4@users.noreply.github.com>
2022-11-22 14:40:18 +0100

    Estructura gespresweb

---
+++ js/gestionpresupuestoweb.js
@@ -0,0 +1,30 @@
+import{
+    mostrarPresupuesto,
+    actualizarPresupuesto,
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos 
+} from './gestionPresupuesto.js';
+
+function mostrarDatoEnId(){
+   
+}
+
+function mostrarGastoWeb(){
+    
+}
+
+function mostrarGastosAgrupadosWeb(){
+
+}
+
+export{
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+}
