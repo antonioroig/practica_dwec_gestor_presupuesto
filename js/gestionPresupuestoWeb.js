db2de739af789d41888d6d6810058e959207f208
Alexis-Reillo-Segarra <alexisreillo12@gmail.com>
2022-11-22 20:44:54 +0100

    2022-11-22

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -11,6 +11,29 @@ function mostrarGastoWeb(idElemento, gasto)
     let elemento = document.getElementById(idElemento);
     let DIVgasto = document.createElement('div');
     DIVgasto.className = 'gasto';
+    let DIVdescripcion = document.createElement('div');
+    DIVdescripcion.className = 'gasto-descripcion';
+    DIVdescripcion.innerHTML = gasto.descripcion;
+    let DIVfecha = document.createElement('div');
+    DIVfecha.className = 'gasto-fecha';
+    DIVfecha.innerHTML = gasto.fecha
+    let DIVvalor = document.createElement('div');
+    DIVvalor.className = 'gasto-valor';
+    DIVvalor.innerHTML = gasto.valor;
+    let DIVetiquetas = document.createElement('div');
+    DIVetiquetas.className = 'gasto-etiquetas';
+    DIVgasto.appendChild(DIVdescripcion);
+    DIVgasto.appendChild(DIVfecha);
+    DIVgasto.appendChild(DIVvalor);
+    gasto.etiquetas.forEach(etiqueta => {
+        let SPANetiqueta = document.createElement('span');
+        SPANetiqueta.className = 'gasto-etiquetas-etiqueta';
+        SPANetiqueta.innerHTML = etiqueta;
+        DIVetiquetas.appendChild(SPANetiqueta);
+    });
+    DIVgasto.appendChild(DIVetiquetas);
+    elemento.appendChild(DIVgasto);
+    return elemento;
 }
 function mostrarGastosAgrupadosWeb()
 {
