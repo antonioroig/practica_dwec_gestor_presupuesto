371817a6cf665b31dcbb754f8da78c1424be0dcf
Pablo Berruezo <pabbergal@alu.edu.gva.es>
2022-11-22 20:44:35 +0100

    Continuacion practica 4 mostrarGastoWEb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -2,9 +2,17 @@
 import * as pres from './gestionPresupuesto.js';
 function mostrarDatoEnId(valor,idElemento)
 {
-    let id = document.getElementById(idElemento);
-    id.innerHTML = valor;
+    idElemento = document.getElementById('presupuesto');
+    valor = pres.mostrarPresupuesto(idElemento);
+    idElemento.innerHTML = valor;
 };
-function mostrarGastoWeb(){};
+function mostrarGastoWeb(gasto)
+{
+    pres.filtrarGastos(gasto);
+    let div = document.createElement('div');
+    div.className = "gasto";
+    div.innerHTML =( "<div class=gasto-descripcion>"+ gasto.descripcion +"</div><div class=gasto-fecha>"+ gasto.fecha
+    +"FECHA DEL GASTO</div> <div class=gasto-valor>"+ gasto.valor +"</div>")
+};
 function mostrarGastosAgrupadosWeb(){};
 export{mostrarDatoEnId,mostrarGastoWeb,mostrarGastosAgrupadosWeb}
