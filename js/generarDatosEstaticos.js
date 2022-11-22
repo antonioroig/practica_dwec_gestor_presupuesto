d9982f098f33206d5864ba58bfa8f0f5a7a24019
Pol Aran <93783980+AreSzb@users.noreply.github.com>
2022-11-22 22:23:24 +0100

    P4 Finalizada

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,6 +1,8 @@
 import * as gp from "./gestionPresupuesto.js";
 import * as gpw from "./gestionPresupuestoWeb.js";
 
+
+
 gp.actualizarPresupuesto(1500);
 
 gpw.mostrarDatoEnId(gp.mostrarPresupuesto(), "presupuesto");
