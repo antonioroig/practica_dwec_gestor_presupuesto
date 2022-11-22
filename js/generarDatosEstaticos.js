4f6f8f1c1e0ad3c52f12da75e44ec8b62856d1f3
Aarón <SanSteAaron@gmail.com>
2022-11-22 19:00:05 +0100

    arreglandoTestRemotos

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,2 @@
+import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js';
+import * as gestionPresupuesto from './gestionPresupuesto.js';
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,14 +1,19 @@
-import * as gestionPresupuesto from '.gestionPresupuesto';
+import * as gestionPresupuesto from './gestionPresupuesto';
 function mostrarDatoEnId(idElemento, valor)
 {
+    gestionPresupuesto.actualizarPresupuesto(valor);
     let element = document.getElementById(idElemento);
-    element.valor = valor;
+    element.innerHTML = gestionPresupuesto.mostrarPresupuesto();
     return element;
 }
 
 function mostrarGastoWeb(idElemento,gasto)
 {
-    
+    let element = document.getElementById(idElemento);
+    let gastoDiv = document.createElement('div');
+    gastoDiv.className = 'gasto';
+    let gastoDes = document.body.firstElementChild.createElement('div');
+
 }
 
 function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo)
