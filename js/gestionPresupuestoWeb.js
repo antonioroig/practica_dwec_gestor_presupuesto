f150d81ba98ff487cb759094d6c8bc28abaaa03c
Aarón <SanSteAaron@gmail.com>
2022-11-22 19:39:05 +0100

    22/11

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -12,13 +12,35 @@ function mostrarGastoWeb(idElemento,gasto)
     let element = document.getElementById(idElemento);
     let gastoDiv = document.createElement('div');
     gastoDiv.className = 'gasto';
-    let gastoDes = document.body.firstElementChild.createElement('div');
-
+    let gastoDes = document.createElement('div');
+    gastoDes.className ='gasto-descripcion';
+    gastoDes.innerHTML = gasto.descripcion;
+    let gastoDate = document.createElement('div');
+    gastoDate.className = 'gasto-fecha';
+    gastoDate.innerHTML = gasto.fecha;
+    let gastoVal = document.createElement('div');
+    gastoVal.className = 'gasto-valor';
+    gastoVal.innerHTML = gasto.valor;
+    let gastoEti = document.createElement('div');
+    gastoEti.className = 'gasto-etiquetas';
+    gastoDiv.appendChild(gastoDes);
+    gastoDiv.appendChild(gastoDate);
+    gastoDiv.appendChild(gastoVal);
+    gastoDiv.appendChild(gastoEti);
+    gasto.etiquetas.forEach(etiqueta => {
+        let spanEti = document.createElement('span');
+        spanEti.className = 'gasto-etiquetas-etiqueta';
+        spanEti.innerHTML = etiqueta;
+        gastoEti.appendChild(spanEti);
+    });
+    element.appendChild(gastoDiv);
+    return element;  
 }
 
 function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo)
 {
-
+    let element = document.getElementById(idElemento);
+    
 }
 
 export{
