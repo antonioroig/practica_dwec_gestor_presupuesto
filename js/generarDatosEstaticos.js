08e39543c211abfa84e6278347ba3aec7b8d81e4
nicmas4 <113632686+nicmas4@users.noreply.github.com>
2022-11-22 14:51:23 +0100

    imports datos estaticos

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,19 @@
+import{
+    mostrarDatoEnId,
+    mostrarGastoWeb,
+    mostrarGastosAgrupadosWeb
+} from './gestionPresupuestoWeb.js';
+
+import{
+    mostrarPresupuesto,
+    actualizarPresupuesto,
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance,
+    filtrarGastos,
+    agruparGastos 
+} from './gestionPresupuesto.js';
+
