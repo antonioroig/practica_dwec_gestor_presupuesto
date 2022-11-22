3744a004523e7cddb702edc8ec1d6d8c3377fef4
SergioSeguraD3 <sersegdur@alu.edu.gva.es>
2022-11-22 20:15:09 +0100

    funcion mostrarGastoWeb completada y comienzo GenerarGastosEstaticos.js

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,2 +1,23 @@
-import * as Gasto from "./gestionPresupuesto";
+import * as Gasto from "./gestionPresupuesto.js";
 import * as GastoWeb from "./gestionPresupuestoWeb.js";
+
+Gasto.actualizarPresupuesto(1500);
+
+GastoWeb.mostrarDatoEnId(Gasto.mostrarPresupuesto(),'presupuesto');
+
+let gasto1 = new Gasto.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
+let gasto2 = new Gasto.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
+let gasto3 = new Gasto.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
+let gasto4 = new Gasto.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
+let gasto5 = new Gasto.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
+let gasto6 = new Gasto.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+Gasto.anyadirGasto(gasto1);
+Gasto.anyadirGasto(gasto2);
+Gasto.anyadirGasto(gasto3);
+Gasto.anyadirGasto(gasto4);
+Gasto.anyadirGasto(gasto5);
+Gasto.anyadirGasto(gasto6);
+
+GastoWeb.mostrarDatoEnId(Gasto.calcularTotalGastos(), 'gastos-totales');
+GastoWeb.mostrarDatoEnId(Gasto.calcularBalance(),'balance-total');
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,4 +1,4 @@
-import * as Gasto from "./gestionPresupuesto";
+import * as gasto from "./gestionPresupuesto";
 
 function mostrarDatoEnId(id, valor)
 {
@@ -22,12 +22,46 @@ function mostrarGastoWeb(id, gasto)
 
         gastoDiv.className = "gasto";
 
+
         let divDescripcionPorGasto = document.createElement('div');
 
         divDescripcionPorGasto.className = "gasto-descripción";
         divDescripcionPorGasto.innerHTML += gasto.descripcion;
 
         gastoDiv.append(divDescripcionPorGasto);
+
+
+        let divFechaPorGasto = document.createElement('div');
+
+        divFechaPorGasto.className = "gasto-fecha";
+        divFechaPorGasto.innerHTML += gasto.fecha;
+
+        gastoDiv.append(divFechaPorGasto);
+
+
+        let divValorPorGasto = document.createElement('div');
+
+        divValorPorGasto.className = "gasto-valor";
+        divValorPorGasto.innerHTML += gasto.valor;
+
+        gastoDiv.append(divValorPorGasto);
+
+
+        let divEtiquetaPorGasto = document.createElement('div');
+
+        for(let i = 0; i < gasto.etiquetas.length; i++)
+        {
+
+            let divEtiquetaNuevaGasto = document.createElement('span');
+
+            divEtiquetaNuevaGasto.className = "gasto-etiquetas-etiqueta";
+            divEtiquetaNuevaGasto.innerHTML += gasto.etiquetas[i];
+
+            gastoDiv.append(divEtiquetaNuevaGasto);
+
+        }
+        gastoDiv.append(divEtiquetaPorGasto);
+        documento.append(gastoDiv)
 
     }
 
