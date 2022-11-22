647d8bffc6669a9ed6222e071eb6936781b8b39a
Aurora Alemañ <auralefus@alu.edu.gva.es>
2022-11-22 20:05:18 +0100

    Presupuesto web

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -6,7 +6,7 @@ function mostrarDatoEnId(idElemento,valor){
     idE.innerHTML = valor;
 };
 
-function mostrarGastoWeb(idElemento, gasto){
+function mostrarGastoWeb(idElemento,gasto){
     let id = document.getElementById(idElemento);
 
     let divGastos = document.createElement('div');
@@ -31,16 +31,27 @@ function mostrarGastoWeb(idElemento, gasto){
     let divEtiq = document.createElement('div');
     divEtiq.className = 'gasto-etiquetas';
     for(let etiqueta of gasto.etiquetas){
-        let spanEtiq = document.createElement('span');
-        spanEtiq.className = 'gasto-etiquetas-etiqueta';
-        spanEtiq.textContent = etiqueta;
-        divEtiq.append(spanEtiq);
+        let span = document.createElement('span');
+        span.className = 'gasto-etiquetas-etiqueta';
+        span.textContent = etiqueta;
+        divEtiq.append(span);
     }
     divGastos.append(divEtiq);
 };
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
+    let idAgrup = document.getElementById(idElemento);
+    idAgrup.innerHTML = '';
 
+    let div = `<div class="agrupacion"> <h1>Gastos agrupados por ${periodo}</h1>`;
+    for(let agrupacion in agrup){
+        div = `<div class="agrupacion-dato">
+            <span class="agrupacion-dato-clave">${agrupacion}</span>
+            <span class="agrupacion-dato-valor">${agrup[agrupacion]}</span>
+            </div>`;
+    }
+    div = '</div>';
+    idAgrup.innerHTML = div;
 };
 
 
