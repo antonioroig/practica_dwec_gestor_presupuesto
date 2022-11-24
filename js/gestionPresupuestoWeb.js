b5d86ed152f03f964f83ae4068cd2d1d3700d670
Pablo Berruezo <pabbergal@alu.edu.gva.es>
2022-11-24 20:13:32 +0100

    Terminado  mostrarGastoWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,23 +1,44 @@
 
 import * as pres from './gestionPresupuesto.js';
+"use strict";
 function mostrarDatoEnId(valor,idElemento)
 {
-    idElemento = document.getElementById('presupuesto');
-    valor = pres.mostrarPresupuesto(idElemento);
-    idElemento.innerHTML = valor;
+    let id = document.getElementById(idElemento);
+    id.innerHTML += valor;
 };
 function mostrarGastoWeb(idElemento,gasto)
 {
  let id = document.getElementById(idElemento);
  let div = document.createElement('div');
- div.classList ="gasto";
+ div.className ="gasto";
  id.appendChild(div);
  let idGasto = document.getElementsByClassName("gasto");
 
  let desc_gasto = document.createElement('div');
- desc_gasto.classList = "gasto-descripción";
+ desc_gasto.className = "gasto-descripcion";
  desc_gasto.textContent = gasto.descripcion;
  id.appendChild(desc_gasto);
+
+ let fecha_gasto = document.createElement('div');
+ fecha_gasto.className = "gasto-fecha";
+ fecha_gasto.textContent = gasto.fecha;
+ id.appendChild(fecha_gasto);
+
+ let valor_gasto = document.createElement('div');
+ valor_gasto.className = "gasto-valor";
+ valor_gasto.textContent = gasto.valor;
+ id.appendChild(valor_gasto);
+
+ let etiquetas_gasto = document.createElement('div');
+ etiquetas_gasto.className = "gasto-etiquetas";
+ for(let etiqueta of gasto.etiquetas)                    
+ {
+    let span = document.createElement('span');
+    span.className = "gasto-etiquetas-etiqueta";
+    span.textContent = etiqueta;
+    etiquetas_gasto.appendChild(span);
+ }
+ id.appendChild(etiquetas_gasto);
 };
 function mostrarGastosAgrupadosWeb(){};
 export{mostrarDatoEnId,mostrarGastoWeb,mostrarGastosAgrupadosWeb}
