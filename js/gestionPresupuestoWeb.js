d02460ef55006e7ad43517443bed693c829cb122
PabloRodera <pabroddec@alu.edu.gva.es>
2022-11-24 20:42:02 +0100

    20221124 modificaciones en gestionPresupuestoWeb, primeras fases de las funciones y construcción del documento generarDatosEstaticos

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -62,7 +62,46 @@ function mostrarGastoWeb(idElemento, gasto)
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo)
 {
+    let elem = document.getElementById(idElemento);
 
+    let divAgrupacion = document.createElement('div');
+
+    divAgrucpacion.className = 'agrupacion';
+
+    let titulo = document.createElement('h1');
+
+    titulo.textContent = 'Gastos agrupados por' + periodo;
+
+    divAgrupacion.appendChild(titulo);
+
+    Object.getOwnPropertyNames(agrup).forEach(function(fecha)
+    {
+        let divDato = document.createElement('div');
+
+        divDato.className = 'agrupacion-dato';
+
+        divAgrupacion.appendChild(divDato);
+
+        let spanClave = document.createElement('span');
+
+        spanClave.className = 'agrupacion-dato-clave';
+
+        spanClave.textContent = fecha;
+
+        divDato.appendChild(spanClave);
+
+        let spanValor = document.createElement('span');
+
+        spanValor.className = 'agrupacion-dato-valor';
+
+        spanValor.textContent = agrup[fecha];
+
+        divDato.appendChild(spanValor);
+    });
+
+    elem.appendChild(divAgrupacion);
+
+    return elem;
 }
 
 export {
