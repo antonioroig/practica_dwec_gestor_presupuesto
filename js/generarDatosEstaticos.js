81f6993ae586c7d9b3759957db6ec0c5bf91fe71
Nicolas <nicmasfor@alu.edu.gva.es>
2022-11-24 12:12:05 +0100

    more fixes

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,9 +1,9 @@
 import * as gpw from './gestionPresupuestoWeb.js';
-import * as gp from './gestionPresupuesto.js'
+import * as gp from './gestionPresupuesto.js';
 
-gp.actualizarPresupuesto(1500)
+gp.actualizarPresupuesto(1500);
 
-gpw.mostrarDatoEnId(gp.mostrarPresupuesto(), "presupuesto")
+gpw.mostrarDatoEnId(gp.mostrarPresupuesto(), "presupuesto");
 
 let gasto1 = new gp.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
 let gasto2 = new gp.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
@@ -19,13 +19,13 @@ gp.anyadirGasto(gasto4);
 gp.anyadirGasto(gasto5);
 gp.anyadirGasto(gasto6);
 
-gpw.mostrarDatoEnId(gp.calcularTotalGastos(), "gastos-totales")
-gpw.mostrarDatoEnId(gp.calcularBalance(), "balance-total")
-gpw.mostrarGastoWeb("listado-gastos-completo", gp.listarGastos())
-gpw.mostrarGastoWeb("listado-gastos-filtrado-1", gp.filtrarGastos(fechaDesde = "2021-09-1", fechaHasta = "2021-09-30"))
-gpw.mostrarGastoWeb("listado-gastos-filtrado-2", gp.filtrarGastos(valorMinimo = 50))
-gpw.mostrarGastoWeb("listado-gastos-filtrado-3", gp.filtrarGastos(valorMinimo = 200, etiquetasTiene = "seguros "))
-gpw.mostrarGastoWeb("listado-gastos-filtrado-4", gp.filtrarGastos(valorMaximo = 50, etiquetasTiene = "comida transporte"))
-gpw.mostrarGastosAgrupadosWeb("agrupacion-dia",gp.agruparGastos("dia"),"dia")
-gpw.mostrarGastosAgrupadosWeb("agrupacion-dia",gp.agruparGastos("mes"),"mes")
-gpw.mostrarGastosAgrupadosWeb("agrupacion-dia",gp.agruparGastos("anyo"),"anyo")
+gpw.mostrarDatoEnId(gp.calcularTotalGastos(), "gastos-totales");
+gpw.mostrarDatoEnId(gp.calcularBalance(), "balance-total");
+gpw.mostrarGastoWeb("listado-gastos-completo", gp.listarGastos());
+gpw.mostrarGastoWeb("listado-gastos-filtrado-1", gp.filtrarGastos(fechaDesde = "2021-09-1", fechaHasta = "2021-09-30"));
+gpw.mostrarGastoWeb("listado-gastos-filtrado-2", gp.filtrarGastos(valorMinimo = 50));
+gpw.mostrarGastoWeb("listado-gastos-filtrado-3", gp.filtrarGastos(valorMinimo = 200, etiquetasTiene = "seguros "));
+gpw.mostrarGastoWeb("listado-gastos-filtrado-4", gp.filtrarGastos(valorMaximo = 50, etiquetasTiene = "comida transporte"));
+gpw.mostrarGastosAgrupadosWeb("agrupacion-dia",gp.agruparGastos("dia"),"dia");
+gpw.mostrarGastosAgrupadosWeb("agrupacion-dia",gp.agruparGastos("mes"),"mes");
+gpw.mostrarGastosAgrupadosWeb("agrupacion-dia",gp.agruparGastos("anyo"),"anyo");
