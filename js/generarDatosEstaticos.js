1236e86366f5f5a67fe0c5a247caa87a1bea4fce
Pablo Berruezo <pabbergal@alu.edu.gva.es>
2022-11-25 16:37:50 +0100

    Avance en GenerarDatosEstaticos hasta fltrar-gastos-1

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -12,3 +12,28 @@ let gasto3=presupuesto.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte")
 let gasto4=presupuesto.CrearGasto("Gasolina", 60.42, "2021-10-08", "transporte", "gasolina");
 let gasto5=presupuesto.CrearGasto("Seguro hogar", 206.45, "2021-09-26", "casa", "seguros");
 let gasto6=presupuesto.CrearGasto("Seguro coche", 195.78, "2021-10-06", "transporte", "seguros");
+
+presupuesto.anyadirGasto(gasto1);
+presupuesto.anyadirGasto(gasto2);
+presupuesto.anyadirGasto(gasto3);
+presupuesto.anyadirGasto(gasto4);
+presupuesto.anyadirGasto(gasto5);
+presupuesto.anyadirGasto(gasto6);
+
+let gastos_totales = presupuesto.calcularTotalGastos();
+web.mostrarDatoEnId(gastos_totales,'gastos-totales');
+
+let balance_total = presupuesto.calcularBalance();
+web.mostrarDatoEnId(balance_total,'balance-total');
+
+let listado_gastos = presupuesto.listarGastos();
+for(let gasto of listado_gastos)
+{
+    web.mostrarGastoWeb('listado-gastos-completo',gasto);
+}
+
+let gastos_septiembre = presupuesto.filtrarGastos({fechaHasta:"2021-09-30",fechaDesde:"2021-09-01"});
+for(let gasto of gastos_septiembre)
+{
+    web.mostrarGastoWeb('listado-gastos-filtrado-1',gasto);
+}
