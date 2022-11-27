99b4ff7a2f480ad20b77badd5f3cbe6bc7b14e3b
Raul Gonzalez <raugonbon@alu.edu.gva.es>
2022-11-27 16:24:54 +0100

    P5 - save

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -42,7 +42,7 @@ function mostrarGastoWeb(idElemento, gasto){
         let divEtiquetas = document.createElement('div');
         divEtiquetas.className = 'gasto-etiquetas';
         
-       for(let i = 0; i < gasto.etiquetas.length; i++){
+        for(let i = 0; i < gasto.etiquetas.length; i++){
             // gasto-etiquetas-etiqueta
             let contenidoEtiqueta = gasto.etiquetas[i];
             let spanEtiqueta = document.createElement('span');
@@ -50,10 +50,9 @@ function mostrarGastoWeb(idElemento, gasto){
             spanEtiqueta.textContent = contenidoEtiqueta + ' ';
             divEtiquetas.append(spanEtiqueta);   
         }
-       
         divGasto.append(divEtiquetas);
         // - - - - - - - - - - - - - -
-       
+
         elemento.append(divGasto);
     
     }
@@ -105,7 +104,7 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
         <div class="agrupacion-dato">
             <span class="agrupacion-dato-clave">2021-09</span>
             <span class="agrupacion-dato-valor">5</span>
-         </div>
+        </div>
 
         <div class="agrupacion-dato">
             <span class="agrupacion-dato-clave">2021-10</span>
@@ -131,78 +130,33 @@ function repintar(){
     document.getElementById('gastos-totales').innerHTML = '';
     document.getElementById('balance-total').innerHTML = '';
 
-    // El presupuesto --> Mostrar el presupuesto en div#presupuesto (funciones mostrarPresupuesto y mostrarDatoEnId)
     mostrarDatoEnId('presupuesto', gestionPresupuesto.mostrarPresupuesto());
-    // - El total de gastos --> Mostrar los gastos totales en div#gastos-totales (funciones calcularTotalGastos y mostrarDatoEnId)
     mostrarDatoEnId('gastos-totales', gestionPresupuesto.calcularTotalGastos());
-    // - El balance actual --> Mostrar el balance total en div#balance-total (funciones calcularBalance y mostrarDatoEnId)
     mostrarDatoEnId('balance-total', gestionPresupuesto.calcularBalance());
 
-     // Borrar el contenido de div#listado-gastos-completo, para que el paso siguiente no duplique la información. Puedes utilizar innerHTML para borrar el contenido de dicha capa.
     document.getElementById('listado-gastos-completo').innerHTML = '';
-    // - Mostrar el listado completo de gastos en div#listado-gastos-completo (funciones listarGastos y mostrarGastoWeb)
+
     let gastosListados = gestionPresupuesto.listarGastos();
     for(let i = 0; i < gastosListados.length; i++){
         gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-completo', gastosListados[i]);
     }
-
-    // - Otra información (agrupaciones de gastos, etc.)
-
-    document.getElementById('listado-gastos-filtrado-1').innerHTML = '';
-
-    let gastosFiltrados = gestionPresupuesto.filtrarGastos({fechaDesde: '2021-09-01', fechaHasta: '2021-09-30'});
-    for(let i = 0; i < gastosFiltrados.length; i++){
-        gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-1', gastosFiltrados[i]);
-    }
-
-    document.getElementById('listado-gastos-filtrado-2').innerHTML = '';
-
-    gastosFiltrados = gestionPresupuesto.filtrarGastos({valorMinimo: 50});
-    for(let i = 0; i < gastosFiltrados.length; i++){
-        gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-2', gastosFiltrados[i]);
-    }
-
-    document.getElementById('listado-gastos-filtrado-3').innerHTML = '';
-
-    gastosFiltrados = gestionPresupuesto.filtrarGastos({valorMaximo: 200, etiquetasTiene: ['seguros']});
-    for(let i = 0; i < gastosFiltrados.length; i++){
-        gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-3', gastosFiltrados[i]);
-    }
-
-    document.getElementById('listado-gastos-filtrado-4').innerHTML = '';
-
-    gastosFiltrados = gestionPresupuesto.filtrarGastos({valorMaximo: 50, etiquetasTiene: ['comida','transporte']})
-    for(let i = 0; i < gastosFiltrados.length; i++){
-        gestionPresupuestoWeb.mostrarGastoWeb('listado-gastos-filtrado-4', gastosFiltrados[i]);
-    }
-
-    let agrupDia = gestionPresupuesto.agruparGastos('dia');
-    let agrupMes = gestionPresupuesto.agruparGastos('mes');
-    let agrupAnyo = gestionPresupuesto.agruparGastos('anyo');
-
-    document.getElementById('agrupacion-dia').innerHTML='';
-    gestionPresupuestoWeb.mostrarGastosAgrupadosWeb('agrupacion-dia',agrupDia, 'día');
-
-    document.getElementById('agrupacion-mes').innerHTML='';
-    gestionPresupuestoWeb.mostrarGastosAgrupadosWeb('agrupacion-mes',agrupMes, 'mes');
-
-    document.getElementById('agrupacion-anyo').innerHTML='';
-    gestionPresupuestoWeb.mostrarGastosAgrupadosWeb('agrupacion-anyo',agrupAnyo, 'año');
 }
 
 /*let element = document.getElementById("actualizarpresupuesto");
 element.addEventListener("click", actualizarPresupuestoWeb());*/
 
+// actualizarPresupuestoWeb
 function actualizarPresupuestoWeb(){
-        gestionPresupuesto.actualizarPresupuesto(parseFloat(prompt('Introduce un presupuesto: ')));
+        let presupuestoNuevo = parseFloat(prompt('Introduce un presupuesto: '));
+        gestionPresupuesto.actualizarPresupuesto(presupuestoNuevo);
         repintar();
 }
-
+// nuevoGastoWeb
 function nuevoGastoWeb()
 {
     let desc = prompt('Introduce la descripción: ');
     let valor = parseFloat(prompt('Introduce el valor: '));
-    let fecha = prompt('Introduce una fecha (aaaa-mm-dd): ');
+    let fecha = prompt('Introduce fecha (aaaa-mm-dd): ');
     let etiquetasTiene = prompt('Introduce las etiquetas: ');
     let etiquetas = etiquetasTiene.split(',');
     let gasto = new gestionPresupuesto.CrearGasto(desc, valor, fecha);
@@ -212,15 +166,15 @@ function nuevoGastoWeb()
     }
 
     gestionPresupuesto.anyadirGasto(gasto);
-
     repintar();
 }
 
-let actualizarpresupuesto = document.getElementById('actualizarpresupuesto');
-actualizarpresupuesto.onclick = actualizarPresupuestoWeb();
+// BUTTONES
+let btnActualizarPresupuesto = document.getElementById('actualizarpresupuesto');
+btnActualizarPresupuesto.onclick = actualizarPresupuestoWeb;
 
-let anyadirgasto = document.getElementById('anyadirgasto');
-anyadirgasto.onclick = nuevoGastoWeb();
+let btnAnyadirGasto = document.getElementById('anyadirgasto');
+btnAnyadirGasto.onclick = nuevoGastoWeb;
 
 // npx cypress open -- PARA HACER TEST GRÁFICO
 // npm run test --> pasa todos los tests
@@ -229,9 +183,11 @@ export   {
     mostrarDatoEnId,
     mostrarGastoWeb,
     mostrarGastosAgrupadosWeb,
+
     repintar,
     actualizarPresupuestoWeb,
     nuevoGastoWeb,
+
 }
 
 
