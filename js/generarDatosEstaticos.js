19b10502d039d38ac5c2b09d388ee27980745d5c
Juan José Hernández <juahermoj@alu.edu.gva.es>
2022-11-29 16:00:15 +0100

    Funciona algo al fin

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -2,6 +2,7 @@ import * as gestionPresupuesto from '../js/gestionPresupuesto.js';
 import * as gestionPresupuestoWeb from '../js/gestionPresupuestoWeb.js';
 
 gestionPresupuesto.actualizarPresupuesto(1500);
+gestionPresupuestoWeb.mostrarDatoEnId("presupuesto", gestionPresupuesto.mostrarPresupuesto());
 let gasto1 =  new gestionPresupuesto.CrearGasto("Compra carne", 23.44, "2021-10-06", "casa", "comida");
 let gasto2 =  new gestionPresupuesto.CrearGasto("Compra fruta y verdura", 14.25, "2021-09-06", "supermercado", "comida");
 let gasto3 =  new gestionPresupuesto.CrearGasto("Bonobús", 18.60, "2020-05-26", "transporte");
@@ -14,3 +15,30 @@ gestionPresupuesto.anyadirGasto(gasto3);
 gestionPresupuesto.anyadirGasto(gasto4);
 gestionPresupuesto.anyadirGasto(gasto5);
 gestionPresupuesto.anyadirGasto(gasto6);
+
+gestionPresupuestoWeb.mostrarDatoEnId("gastos-totales", gestionPresupuesto.calcularTotalGastos());
+gestionPresupuestoWeb.mostrarDatoEnId("balance-total", gestionPresupuesto.calcularBalance());
+let listaGastos1 = gestionPresupuesto.listarGastos();
+for (let i = 0; i < listaGastos1.length; i++) {
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-completo", listaGastos1[i]);
+}
+
+let listaGastos2 = gestionPresupuesto.filtrarGastos({fechaDesde: "2021-09-01", fechaHasta: "2021-09-30"});
+for (let i = 0; i < listaGastos2.length; i++) {
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-1", listaGastos2[i]);
+}
+
+let listaGastos3 = gestionPresupuesto.filtrarGastos({valorMinimo: 50})
+for (let i = 0; i < listaGastos3.length; i++) {
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-2", listaGastos3[i]);
+}
+
+let listaGastos4 = gestionPresupuesto.filtrarGastos({valorMinimo: 200});
+for (let i = 0; i < listaGastos4.length; i++) {
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-3", listaGastos4[i]);
+}
+
+let listaGastos5 = gestionPresupuesto.filtrarGastos({valorMaximo: 50, etiquetasTiene: ["comida", "transporte"]})
+for (let i = 0; i < listaGastos5.length; i++) {
+    gestionPresupuestoWeb.mostrarGastoWeb("listado-gastos-filtrado-4", listaGastos5[i]);
+}
--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,15 +1,92 @@
-function mostrarDatoEnId(valor, idElemento) {
-    let div = document.createElement('div');
-    div.className = idElemento;
-    div.innerHTML = valor;
+import * as gestionPresupuesto from '../js/gestionPresupuesto.js';
+
+function mostrarDatoEnId(idElemento, valor) {
+    if(idElemento != undefined) {
+        let div = document.getElementById(idElemento);
+        div.innerHTML += "" + valor;
+    }
 }
 
 function mostrarGastoWeb(idElemento, gasto) {
+    if(idElemento != undefined){
+        let div = document.getElementById(idElemento);
+
+        let divgasto = document.createElement('div');
+        divgasto.className = "gasto";
+
+        let gastodes = document.createElement('div');
+        gastodes.className = "gasto-descripcion";
+        gastodes.innerHTML = gasto.descripcion;
+        divgasto.append(gastodes);
+
+        let gastofecha = document.createElement('div');
+        gastofecha.className = "gasto-fecha";
+        let fecha1 = new Date(gasto.fecha);
+        let txtfecha = fecha1.toLocaleString();
+        gastofecha.innerHTML = txtfecha;
+        divgasto.append(gastofecha);
+
+        let gastovalor = document.createElement('div');
+        gastovalor.className = "gasto-valor";
+        gastovalor.innerHTML = gasto.valor;
+        divgasto.append(gastovalor);
+
+        let gastoeti = document.createElement('div');
+        gastoeti.className = "gasto-etiquetas";
+        
+        //if (gasto.etiquetas.length != undefined) {
+        for (let i = 0; i < gasto.etiquetas.length; i++) {
+            let eti = document.createElement('span');
+            eti.className = "gasto-etiquetas-etiqueta";
+            eti.innerHTML = gasto.etiquetas[i];
+            gastoeti.append(eti);
+        }                       
+        //}
+
+        divgasto.append(gastoeti);
+        div.append(divgasto);       
+    }
 
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
-    
+    let div = document.getElementById(idElemento);
+
+    let agrupacion = document.createElement('div');
+    agrupacion.className = "agrupacion";
+
+    let encabezado = document.createElement('h1');
+    encabezado.innerHTML = "Gasto agrupados por " + periodo;
+    agrupacion.append(encabezado);
+
+    for (let i = 0; i < agrup.length; i++) {
+        let agrupaciondato = document.createElement('div');
+        agrupaciondato.className = "agrupacion-dato";
+
+        let dataclave = document.createElement('span');
+        dataclave.className = "agrupacion-dato-clave";
+        let Arrclaves =  Object.keys(agrup[i]);
+        let claves;
+        for (let i = 0; i < Arrclaves.length; i++) {
+            claves += Arrclaves[i] + "\n";
+        }
+        dataclave.innerHTML(claves);
+
+        let datavalor = document.createElement('span');
+        datavalor.className = "agrupacion-dato-valor";
+        let ArrValores = Object.values(agrup[i]);
+        let valores;
+        for (let i = 0; i < ArrValores.length; i++) {
+            valores += ArrValores[i] + "\n";
+        }
+        datavalor.innerHTML(valores);
+
+        agrupaciondato.append(dataclave);
+        agrupaciondato.append(datavalor);  
+    }
+
+    agrupacion.append(agrupaciondato);
+    div.append(agrupacion);
 }
 
 export   {
