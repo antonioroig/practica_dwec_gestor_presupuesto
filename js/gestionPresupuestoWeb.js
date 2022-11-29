aca779da8cf2d0d2004c8f816c06786d511c87c9
CalderonSerrano <joscalser@alu.edu.gva.es>
2022-11-29 18:43:07 +0100

    modificando mostrarGastoWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -8,6 +8,9 @@ function mostrarDatoEnId(valor,idElemento) {
     parrafo.textContent = valor;
     elemento.append(parrafo);
 }
+
+/*
+Primer mostrarGastoWeb
 function mostrarGastoWeb(idElemento, gasto) {
     let elemento2 = document.getElementById(idElemento);
 
@@ -44,6 +47,81 @@ function mostrarGastoWeb(idElemento, gasto) {
         divGastoEtiquetas.appendChild(spanEtiqueta);
     }
     elemento2.appendChild(divGasto);
+}
+*/
+
+// mostrarGastoWeb modificado (práctica eventos)
+function mostrarGastoWeb(idElemento, gasto) {
+    let elemento2 = document.getElementById(idElemento);
+
+    //Creamos un div con class="gasto".
+    let divGasto = document.createElement('div');
+    divGasto.className += 'gasto';                                                                       
+
+    let divDescripcion  = document.createElement('div');
+    divDescripcion.className = 'gasto-descripcion'; 
+    divDescripcion.textContent = gasto.descripcion;
+    //Para decir que está dentro del div divGasto:
+    divGasto.appendChild(divDescripcion);
+
+    let divFecha  = document.createElement('div');
+    divFecha.className = 'gasto-fecha'; 
+    divFecha.textContent = new Date(gasto.fecha).toLocaleDateString();
+    divGasto.appendChild(divFecha);
+
+    let divValor = document.createElement('div');
+    divValor.className = 'gasto-valor';
+    divValor.textContent = gasto.valor;
+    divGasto.appendChild(divValor);
+
+    let divGastoEtiquetas = document.createElement('div');
+    divGastoEtiquetas.className = 'gasto-etiquetas';
+    divGasto.appendChild(divGastoEtiquetas);
+    
+    //Ahora necesitamos un bucle para recorrer los gastos
+    for(let i = 0; i < gasto.etiquetas.length; i++)
+    {
+        let spanEtiqueta = document.createElement('span');
+        spanEtiqueta.className = 'gasto-etiquetas-etiqueta';
+        spanEtiqueta.textContent =  `${gasto.etiquetas[i]}\n`;
+        divGastoEtiquetas.appendChild(spanEtiqueta);
+    }
+    elemento2.appendChild(divGasto);
+
+    // boton editar
+    let btnEditar = document.createElement('button');
+    btnEditar.textContent = 'Editar';
+    btnEditar.type = 'button';
+    btnEditar.className = 'gasto-editar';
+
+    // objeto editar
+    let objetoEditar = new EditarHandle();
+    objetoEditar.gasto = gasto;
+    btnEditar.addEventListener('click', objetoEditar);   
+    
+    divGasto.append(btnEditar);
+
+    // boton borrar
+    let btnBorrar = document.createElement('button');
+    btnBorrar.textContent = 'Borrar';
+    btnBorrar.type = 'button';
+    btnBorrar.className = 'gasto-borrar';
+
+    // objeto borrar
+    let objetoBorrar = new BorrarHandle();
+    objetoBorrar.gasto = gasto;
+    btnBorrar.addEventListener('click', objetoBorrar);
+
+    divGasto.append(btnBorrar);
+
+
+    for(let i = 0; i < gasto.etiquetas.length; i++)
+    {
+        let borrarEtiquetas = new BorrarEtiquetasHandle();
+            borrarEtiquetas.gasto = gasto;
+            borrarEtiquetas.etiqueta = gasto.etiquetas[i];
+    }
+    
 }
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
     let elemento3 = document.getElementById(idElemento);
