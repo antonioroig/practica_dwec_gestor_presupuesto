1f0d8e55bdbb296c4d758581885b10ef381b1f7d
jormonbla <jormonbla@alu.edu.gva.es>
2022-12-01 18:47:54 +0100

    pr1finalizada

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,53 @@
-// TODO: Crear las funciones, objetos y variables indicadas en el enunciado
+let presupuesto = 0;
 
-// TODO: Variable global
+function actualizarPresupuesto(a) {
 
-
-function actualizarPresupuesto() {
-    // TODO
+    if(a >= 0 && typeof a === "number"){
+        presupuesto = a;
+        return a;
+    }
+    else
+    {
+        
+        console.log(`el número ${a} no es valido `);
+        return -1;
+    }
+    return presupuesto;
 }
+
+//Revisar
 
 function mostrarPresupuesto() {
-    // TODO
+    return `Tu presupuesto actual es de ${presupuesto} €`;
 }
+// revisar
+function CrearGasto(Descripcion,Valor) {
+    this.descripcion = Descripcion;
 
-function CrearGasto() {
-    // TODO
+    if(Valor > 0 && ! isNaN(Valor)){
+
+        this.valor = Valor;
+
+    }else{
+        this.valor = 0;
+    };
+
+    this.mostrarGasto = function (){
+        return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`;
+    };
+
+    this.actualizarDescripcion = function(cadena){
+        this.descripcion = cadena;
+    };
+
+    this.actualizarValor = function(valorActualizado){
+        if(valorActualizado >= 0){
+            this.valor = valorActualizado
+        }
+        else{
+            console.log(`El valor introducido es negativo, no ha podido ser cambiado`)
+        };
+    };
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
@@ -23,3 +58,4 @@ export   {
     actualizarPresupuesto,
     CrearGasto
 }
+
