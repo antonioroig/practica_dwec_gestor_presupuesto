0648275f5747b85384bc6130893107292b96d157
SergioSeguraD3 <sersegdur@alu.edu.gva.es>
2022-12-01 18:27:07 +0100

    problemas soilucionados, fallos en funciones detectadas y practica casi finalizada

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -16,87 +16,86 @@ function mostrarDatoEnId(valor, idElemento)
 function mostrarGastoWeb(idElemento, gasto)
 {
 
-        let elemento = document.getElementById(idElemento);
-        let divGasto = document.createElement('div');
+    let elemento = document.getElementById(idElemento);
+    let divGasto = document.createElement('div');
 
-        divGasto.className = "gasto";
-        elemento.append(divGasto);
+    divGasto.className = "gasto";
+    let divDescripcionPorGasto = document.createElement('div');
+    divDescripcionPorGasto.className = "gasto-descripcion";
 
-        let divDescripcionPorGasto = document.createElement('div');
+    divDescripcionPorGasto.innerHTML+=gasto.descricpion;
+    divGasto.append(divDescripcionPorGasto);
 
-        divDescripcionPorGasto.className = "gasto-descripcion";
-        divDescripcionPorGasto.innerHTML += gasto.descripcion;
+    let divFechaPorGasto = document.createElement('div');
+    divFechaPorGasto.className = "gasto-fecha";
+    divFechaPorGasto.innerHTML+=gasto.fecha;
+    divGasto.append(divFechaPorGasto);
 
-        divGasto.append(divDescripcionPorGasto);
+    let divValorPorGasto = document.createElement('div');
+    divValorPorGasto.className = "gasto-valor";
+    divValorPorGasto.innerHTML+=gasto.valor;
+    divGasto.append(divValorPorGasto);
 
+    let divEtiquetasPorGasto = document.createElement('div');
+    divEtiquetasPorGasto.className = "gasto-etiquetas";
+    
+    for(let i = 0; i < gasto.etiquetas.length; i++){
 
-        let divFechaPorGasto = document.createElement('div');
+        let divEtiquetaNuevoGasto = document.createElement('span');
 
-        divFechaPorGasto.className = "gasto-fecha";
-        divFechaPorGasto.innerHTML += gasto.fecha;
+        divEtiquetaNuevoGasto.className = "gasto-etiquetas-etiqueta";
+        divEtiquetaNuevoGasto.textContent = gasto.etiquetas[i];
+        
+        let BtnEtiqBorrarHandle = new BorrarEtiquetasHandle();
 
-        divGasto.append(divFechaPorGasto);
+        BtnEtiqBorrarHandle.gasto = gasto;
+        
+        BtnEtiqBorrarHandle.petiquetas = gasto.etiquetas[i];
+        
+        divEtiquetaNuevoGasto.addEventListener("click", BtnEtiqBorrarHandle);
+        
+        divEtiquetaNuevoGasto.textContent = gasto.etiquetas[i] + " ";
 
+        divEtiquetasPorGasto.append(divEtiquetaNuevoGasto);
+    }
+    divGasto.append(divEtiquetasPorGasto);
+    elemento.append(divGasto);
+    
+    
+    let btnEditar = document.createElement("button");
+    
+    btnEditar.className = "gasto-editar";
+    
+    btnEditar.type = "button";
+    
+    btnEditar.innerHTML = "Editar";
 
-        let divValorPorGasto = document.createElement('div');
+    
+    let BtnEditarHandle = new EditarHandle();
+    
+    BtnEditarHandle.gasto = gasto;
+    
+    btnEditar.addEventListener("click", BtnEditarHandle);
+    
+    divGasto.append(btnEditar);
+    
 
-        divValorPorGasto.className = "gasto-valor";
-        divValorPorGasto.innerHTML += gasto.valor;
+    let btnBorrar = document.createElement("button");
+    
+    btnBorrar.className = "gasto-borrar";
+    
+    btnBorrar.type = "button";
+    
+    btnBorrar.innerHTML = "Borrar";
 
-        divGasto.append(divValorPorGasto);
-
-
-        let divEtiquetaPorGasto = document.createElement('div');
-        divEtiquetaPorGasto.className = "gasto-etiquetas";
-
-        for(let etiquetas of gasto.etiquetas)
-        {
-
-            let divEtiquetaNuevaGasto = document.createElement('span');
-
-            divEtiquetaNuevaGasto.className = "gasto-etiquetas-etiqueta";
-            
-            divEtiquetaNuevaGasto.innerHTML = etiquetas;
-            divGasto.append(divEtiquetaPorGasto);
-
-        }
-        elemento.append(divEtiquetaPorGasto);
-
-        let btnEditar = document.createElement("button");
-
-        btnEditar.className = "gasto-editar";
-
-        btnEditar.type = "button";
-
-        btnEditar.innerHTML += "Editar";
-
-
-        let editarHandleBtn = new EditarHandle();
-
-        editarHandleBtn.gasto = gasto;
-
-        btnEditar.addEventListener("click", editarHandleBtn);
-
-        divGasto.append(btnEditar);
-
-
-        let btnBorrar = document.createElement("button");
-
-        btnEditar.className = "gasto-borrar";
-
-        btnEditar.type = "button";
-
-        btnEditar.innerHTML += "Borrar";
-
-
-        let borrarHandleBtn = new BorrarHandle();
-
-        borrarHandleBtn.gasto = gasto;
-
-        btnEditar.addEventListener("click", borrarHandleBtn);
-
-        divGasto.append(btnBorrar);
-
+    
+    let BtnBorrarHandle = new BorrarHandle();
+    
+    BtnBorrarHandle.gasto = gasto;
+    
+    btnBorrar.addEventListener("click", BtnBorrarHandle);
+    
+    divGasto.append(btnBorrar);
     }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrupar, periodo)
@@ -149,21 +148,22 @@ function mostrarGastosAgrupadosWeb(idElemento, agrupar, periodo)
 function repintar()
 {
 
-    document.getElementById('presupuesto');
-    mostrarDatoEnId("presupuesto",gesPresupuesto.mostrarPresupuesto);
 
-    document.getElementById('gastos-totales');
-    mostrarDatoEnId("gastos-totales",gesPresupuesto.calcularTotalGastos);
+    document.getElementById("presupuesto").innerHTML = "";
+   
+    document.getElementById("gastos-totales").innerHTML = "";
+   
+    document.getElementById("balance-total").innerHTML = "";
+   
+    document.getElementById("listado-gastos-completo").innerHTML = "";
 
-    document.getElementById('balance-total');
-    mostrarDatoEnId("balance-total",gesPresupuesto.calcularBalance);
-    
-    document.getElementById('listado-gastos-completo').innerHTML = "";
+    mostrarDatoEnId(gesPresupuesto.mostrarPresupuesto(), "presupuesto");
+    mostrarDatoEnId(gesPresupuesto.calcularTotalGastos(), "gastos-totales");
+    mostrarDatoEnId(gesPresupuesto.calcularBalance(), "balance-total");
+   
+    for(let gasto of gesPresupuesto.listarGastos()){
 
-    for(let gastos of gesPresupuesto.listarGastos())
-    {
-
-        mostrarGastoWeb("listado-gastos-completo", gastos)
+        mostrarGastoWeb("listado-gastos-completo",gasto);
 
     }
 
