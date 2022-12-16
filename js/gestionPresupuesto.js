46d366eb6934eb6b4b2b5a19782d56e1fec1cfdc
JaviGva <javbernav@alu.edu.gva.es>
2022-12-16 18:45:56 +0100

    funcion ransformarListadoEtiquetas terminada

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -199,6 +199,27 @@ function agruparGastos(periodo = 'mes', etiquetas, fechaDesde, fechaHasta = Date
     return objeto;
 }
 
+function transformarListadoEtiquetas(stringIntroducido){
+    var regex = /[\s,:.;~]+/g;
+    var stringCorregido = stringIntroducido.split(regex);
+    /*var stringCorregido = stringIntroducido.replace(" :, ; ", ",");
+    stringCorregido = stringCorregido.replace(" ,   ", ",");
+    stringCorregido = stringCorregido.replace(" , ", ",");
+    stringCorregido = stringCorregido.replace(", ", ",");
+    
+    stringCorregido = stringCorregido.replace(". ", ",");
+    stringCorregido = stringCorregido.replace(".", ",");
+    
+    stringCorregido = stringCorregido.replace(":", ",");
+    stringCorregido = stringCorregido.replace(";", ",");
+    stringCorregido = stringCorregido.replace("    ", ",");
+    
+    stringCorregido = stringCorregido.replace(" ", ",");*/
+    
+    //var array = stringCorregido.split(",");
+    return stringCorregido;
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -212,5 +233,6 @@ export   {
     calcularTotalGastos, 
     calcularBalance,
     filtrarGastos,
-    agruparGastos
+    agruparGastos,
+    transformarListadoEtiquetas
 }
