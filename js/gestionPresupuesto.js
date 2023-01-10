1d4d207156e32bb8820535449ea096dd8280e8bd
Joel Ortega <joel.rosillo@gmail.com>
2023-01-10 20:43:40 +0100

    empezar practica7

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -283,6 +283,10 @@ function agruparGastos(periodo = "mes",etiquetas, fechaDesde,fechaHasta){
     return objetofinal;
 }
 
+function transformarListadoEtiquetas(){
+    let etiquetas = 
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -297,5 +301,6 @@ export   {
     calcularBalance,
     filtrarGastos,
     agruparGastos,
+    transformarListadoEtiquetas
 }
 
