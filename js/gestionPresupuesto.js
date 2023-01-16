3bb6dd1807ebf880361a0cdb268e2c613a6321d7
JaviGva <javbernav@alu.edu.gva.es>
2023-01-16 18:49:29 +0100

    añadido boton y cargarGastos

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -220,6 +220,17 @@ function transformarListadoEtiquetas(stringIntroducido){
     return stringCorregido;
 }
 
+function cargarGastos(gastosAlmacenamiento){
+    gastos = [];
+    for (let g of gastosAlmacenamiento) {
+        let gastoRehidratado = new CrearGasto();
+        
+        Object.assign(gastoRehidratado, g);
+        
+        gastos.push(gastoRehidratado)
+    }
+}
+
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
@@ -234,5 +245,6 @@ export   {
     calcularBalance,
     filtrarGastos,
     agruparGastos,
-    transformarListadoEtiquetas
+    transformarListadoEtiquetas,
+    cargarGastos
 }
