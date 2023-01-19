36456a7385372f9965d82fef281700cb7aed28ad
Andrea <andzoucan@alu.edu.gva.es>
2023-01-19 11:52:19 +0100

    primera funcion creada

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,8 +1,14 @@
 'use strict';
 import * as gestionPresupuesto from  "./gestionPresupuesto.js";
 
-
-
+function mostrarDatosEnId(idElemento, valor)
+{
+    if(idElemento != undefined)
+    {
+        let elemento = document.getElementById(idElemento);
+        elemento.innerHTML += valor;
+    }
+}
 
 
 
