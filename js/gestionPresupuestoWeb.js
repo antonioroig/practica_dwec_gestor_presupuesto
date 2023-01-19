004484a18107e353caecc11e64864b60092729f0
Andrea <andzoucan@alu.edu.gva.es>
2023-01-19 12:16:22 +0100

    funcion mostrargastoweb empezada y avanzada falta acabar

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -10,8 +10,40 @@ function mostrarDatosEnId(idElemento, valor)
     }
 }
 
+function mostrarGastoWeb(idElemento, gasto)
+{
+    if(idElemento != undefined)
+    {
+        let elemento=document.getElementById(idElemento);
+        let divGasto = document.createElement("div");
+        divGasto.className="gasto";
+        elemento.appendChild(divGasto);
 
+        if (gasto.descripcion)
+        {
+            let divGastoDescripcion = document.createElement("div");
+            divGastoDescripcion.className = "gasto-descripcion";
+            divGastoDescripcion.innerHTML += gasto.descripcion;
+            divGasto.appendChild(divGastoDescripcion);
+        }
 
+        if(gasto.valor)
+        {
+            let divGastoValor = document.createElement("div");
+            divGastoValor.className = "gasto-valor";
+            divGastoValor.innerHTML += gasto.valor;
+            divGasto.appendChild(divGastoValor);
+        }
+
+        if (gasto.fecha)
+        {
+            let divGastoFecha = document.createElement("div");
+            divGastoFecha.className = "gasto-fecha";
+            divGastoFecha.innerHTML += gasto.fecha;
+            divGasto.appendChild(divGastoFecha);  
+        }
+    }
+}
 
 
 
