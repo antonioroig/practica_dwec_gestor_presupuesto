58c9ff1f232025c1efcdcadd8170f242b5e44994
Andrea Zoughbi Cano <andzoucan@alu.edu.gva.es>
2023-02-01 18:48:20 +0100

    continuación funcion repintar

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -91,7 +91,10 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo)
 function repintar()
 {
     gestionPresupuestoWeb.mostrarDatoEnId('presupuesto', gestionPresupuesto.mostrarPresupuesto());
+    gestionPresupuestoWeb.mostrarDatoEnId('gastos-totales', gestionPresupuesto.calcularTotalGastos());
+    gestionPresupuestoWeb.mostrarDatoEnId('balance-total', gestionPresupuesto.calcularBalance());
 
+    gestionPresupuestoWeb.mostrarDatoEnId('listado-gastos-completo', gestionPresupuesto.listarGastos());
 }
 
 export{
