3c28b374a354956c5f440e2af2b4be09c9d1fc7a
Aarón <SanSteAaron@gmail.com>
2023-02-07 19:54:45 +0100

    Terminada  practica 10

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -92,6 +92,7 @@ function mostrarGastoWeb(idElemento,gastos)
 function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo)
 {
     let element = document.getElementById(idElemento);
+    element.innerHTML = "";
     let agrupDIV = document.createElement('div');
     agrupDIV.className = 'agrupacion';
     let agrupH1 = document.createElement('h1');
@@ -111,6 +112,66 @@ function mostrarGastosAgrupadosWeb(idElemento,agrup,periodo)
         agrupDIV.appendChild(datoDIV);
     }
     element.appendChild(agrupDIV);
+
+    // Estilos
+    element.style.width = "33%";
+    element.style.display = "inline-block";
+    // Crear elemento <canvas> necesario para crear la gráfica
+    // https://www.chartjs.org/docs/latest/getting-started/
+    let chart = document.createElement("canvas");
+    // Variable para indicar a la gráfica el período temporal del eje X
+    // En función de la variable "periodo" se creará la variable "unit" (anyo -> year; mes -> month; dia -> day)
+    let unit = "";
+    switch (periodo) {
+    case "anyo":
+        unit = "year";
+        break;
+    case "mes":
+        unit = "month";
+        break;
+    case "dia":
+    default:
+        unit = "day";
+        break;
+    }
+
+    // Creación de la gráfica
+    // La función "Chart" está disponible porque hemos incluido las etiquetas <script> correspondientes en el fichero HTML
+    const myChart = new Chart(chart.getContext("2d"), {
+        // Tipo de gráfica: barras. Puedes cambiar el tipo si quieres hacer pruebas: https://www.chartjs.org/docs/latest/charts/line.html
+        type: 'bar',
+        data: {
+            datasets: [
+                {
+                    // Título de la gráfica
+                    label: `Gastos por ${periodo}`,
+                    // Color de fondo
+                    backgroundColor: "#555555",
+                    // Datos de la gráfica
+                    // "agrup" contiene los datos a representar. Es uno de los parámetros de la función "mostrarGastosAgrupadosWeb".
+                    data: agrup
+                }
+            ],
+        },
+        options: {
+            scales: {
+                x: {
+                    // El eje X es de tipo temporal
+                    type: 'time',
+                    time: {
+                        // Indicamos la unidad correspondiente en función de si utilizamos días, meses o años
+                        unit: unit
+                    }
+                },
+                y: {
+                    // Para que el eje Y empieza en 0
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    // Añadimos la gráfica a la capa
+    element.append(chart);
 }
 
 function repintar(){
@@ -120,6 +181,12 @@ function repintar(){
     mostrarDatoEnId('balance-total',gestionPresupuesto.calcularBalance());
     mostrarDatoEnId('listado-gastos-completo',"");
     mostrarGastoWeb('listado-gastos-completo',gestionPresupuesto.listarGastos());
+    mostrarGastosAgrupadosWeb('agrupacion-dia',"");
+    mostrarGastosAgrupadosWeb('agrupacion-mes',"");
+    mostrarGastosAgrupadosWeb('agrupacion-anyo',"");
+    mostrarGastosAgrupadosWeb('agrupacion-dia',gestionPresupuesto.agruparGastos("dia"),"día");
+    mostrarGastosAgrupadosWeb('agrupacion-mes',gestionPresupuesto.agruparGastos("mes"),"mes");
+    mostrarGastosAgrupadosWeb('agrupacion-anyo',gestionPresupuesto.agruparGastos("anyo"),"año");
 }
 
 function actualizarPresupuestoWeb(){
@@ -422,7 +489,7 @@ function EnviarApiHandle(){
             descripcion :descripcion, 
             valor:valor, 
             fecha:fecha, 
-            etiquetas: etiquetas.split(","),
+            etiquetas: etiquetas.split(", "),
         }
         let jsonGasto = JSON.stringify(gasto)
         let user = (document.getElementById("nombre_usuario")).value;
@@ -451,7 +518,7 @@ function EditarHandleApi(){
             descripcion :descripcion, 
             valor:valor, 
             fecha:fecha, 
-            etiquetas: etiquetas.split(","),
+            etiquetas: etiquetas.split(", "),
         }
         
         let jsonGasto = JSON.stringify(gasto)
